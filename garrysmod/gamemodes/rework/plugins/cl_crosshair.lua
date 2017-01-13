4307e67edae933c62e468ec259afc0adb7fbb14a
Alex D <cod7777@yandex.ru>
2017-01-13 17:26:04 +0100

    Fixed crosshair coding style and standard

--- garrysmod/gamemodes/rework/plugins/cl_crosshair.lua
+++ garrysmod/gamemodes/rework/plugins/cl_crosshair.lua
@@ -6,8 +6,8 @@
 
 PLUGIN:SetName("Crosshair");
 PLUGIN:SetAuthor("Mr. Meow");
-PLUGIN:SetDescription("Adds crosshair.");
-local Stored = {};
+PLUGIN:SetDescription("Adds a crosshair.");
+
 CreateClientConVar("cl_crosshair_thickness", "1");
 CreateClientConVar("cl_crosshair_size", "4");
 CreateClientConVar("cl_crosshair_gap", "1");
@@ -21,130 +21,103 @@ CreateClientConVar("cl_crosshair_color_a", "255");
 CreateClientConVar("cl_crosshair_fadein", "0.5");
 CreateClientConVar("cl_crosshair_fadeshow", "1.5");
 
-local startTime = CurTime();
-local curSize 	= nil;
+local startTime 	= CurTime();
+local curSize 		= nil;
 local _alpha 		= nil;
-local _talpha 	= 0;
-local TYPE_WORLD	= 0;
-local TYPE_PLAYER = 1;
-local TYPE_ITEM 	= 2;
-local TYPE_USE 		= 3;
-local UseItems 		= {"prop_door_rotating", "func_door"};
-local ScrW = ScrW;
-local ScrH = ScrH;
+local _talpha 		= 0;
+local doors 		= {"prop_door_rotating", "func_door"};
 local textStartTime = 0;
-local oldObj 				= nil;
-
-Stored[TYPE_PLAYER] = {
-	object = function(item)
-		return item:IsPlayer();
-	end,
-	color = Color(152,190,230),
-	showText = false
-};
-Stored[TYPE_ITEM] = {
-	object = function(item)
-		return (item:GetClass() == "rework_item");
-	end,
-	color = Color(212,176,230),
-	showText = true
-};
-Stored[TYPE_USE] = {
-	object = function(item)
-		return table.HasValue(UseItems, item:GetClass());
-	end,
-	color = Color(240,211,152),
-	showText = true
-};
-Stored[TYPE_WORLD] = {
-	object = function(item)
-		return false;
-	end,
-	color = Color(255,255,255),
-	showText = false
-};
+local prevEnt 		= nil;
 
 function PLUGIN:HUDPaint()
 	if (!plugin.Call("PreDrawCrosshair")) then
-		local drawColor, showText;
+		local scrW, scrH, curTime = ScrW(), ScrH(), CurTime();
 		local trace = rw.client:GetEyeTraceNoCursor();
 		local distance = trace.StartPos:Distance(trace.HitPos);
 		local radius = plugin.Call("AdjustCrosshairRadius", trace, distance) or math.Clamp(4 / distance, 2, 6);
-		drawColor, showText = Color(255, 255, 255, 150), false;
-		drawColor, showText = plugin.Call("AdjustCrosshairColor", trace, distance);
+		local drawColor, showText = plugin.Call("AdjustCrosshairColor", trace, distance);
+
+		if (!drawColor) then return; end;
 
 		surface.SetDrawColor(drawColor);
-		surface.DrawOutlinedCircle(ScrW() / 2, ScrH() / 2, radius, 1, 32, false);
+		surface.DrawOutlinedCircle(scrW / 2, scrH / 2, radius, 1, 32, false);
 
 		if (distance < 70) then
-			if (oldObj != trace.Entity) then
-				oldObj = trace.Entity;
-				textStartTime = CurTime();
-			end
-		end
+			if (prevEnt != trace.Entity) then
+				prevEnt = trace.Entity;
+				textStartTime = curTime;
+			end;
+		end;
+
 		local fadein = GetConVar("cl_crosshair_fadein"):GetFloat();
 		local fadeshow = GetConVar("cl_crosshair_fadeshow"):GetFloat();
-		_talpha = Lerp(FrameTime()*(showText and 4 or 6), _talpha or 0, (showText and drawColor.a < 5 and (255-255/fadein*math.Clamp(CurTime()-fadeshow-textStartTime, 0, fadein)) or 0));
-		draw.SimpleText(L"#TargetID_Action", "menu_thin_smaller", ScrW()/2, ScrH()/2, Color(255,255,255,_talpha), 1, 1);
+
+		_talpha = Lerp(
+			FrameTime() * (showText and 4 or 6), 
+			_talpha or 0, 
+			(
+				showText and drawColor.a < 5 and 
+				255 - 255 / fadein * math.Clamp(curTime - fadeshow - textStartTime, 0, fadein)
+				or 0
+			)
+		);
+
+		draw.SimpleText("#TargetID_Action", "menu_thin_smaller", scrW / 2, scrH / 2, Color(255, 255, 255, _talpha), 1, 1);
 	end;
 end;
 
 function PLUGIN:AdjustCrosshairColor(trace, distance)
-	local r, g, b = 255, 255, 255;
+	local drawColor = Color(255, 255, 255);
 	local alpha = 150;
-	local sText = false;
+	local bShouldDrawText = false;
+	local ent = trace.Entity;
 
 	if (distance > 1000) then
 		alpha = math.Clamp(alpha - (distance - 1000) / 30, 50, 200);
 	end;
 
-	if (distance < 750 and IsValid(trace.Entity) and !trace.Entity:IsWorld()) then
-		local itype = nil;
-		for k,v in pairs(Stored) do
-			local obj = v.object(trace.Entity);
-			if (obj) then
-				itype = k;
-			else
-				if (k == Stored) then
-					itype = TYPE_WORLD;
-				else
-					continue;
-				end;
-			end;
+	if (distance < 300 and IsValid(ent) and !ent:IsWorld()) then
+		if (ent:IsPlayer()) then
+			drawColor = Color(150, 190, 230);
+		elseif (ent:GetClass() == "rework_item") then
+			drawColor = Color(210, 175, 230);
+		elseif (table.HasValue(doors, ent:GetClass())) then
+			drawColor = Color(240, 210, 150);
+			bShouldDrawText = true;
 		end;
-		local titem = Stored[itype] or {color = Color(255,255,255), showText = false};
-		r, g, b = titem.color.r, titem.color.g, titem.color.b;
-		if (distance < 70) then
-			sText = titem.showText;
-		end
 	end;
+
 	if (distance < 70) then
 		alpha = 0;
-	end
+	end;
+
 	if (LocalPlayer():GetVelocity():Length2D() > 150) then
 		alpha = 0;
-	end
+	end;
 
 	_alpha = Lerp(FrameTime() * 4, (_alpha or alpha), alpha);
-	return Color(r, g, b, _alpha), sText;
+
+	return ColorAlpha(drawColor, _alpha), bShouldDrawText;
 end;
 
 function PLUGIN:AdjustCrosshairRadius(trace, distance)
 	local dist = math.Clamp(distance * 3, 200, 2400) / 1000;
-
 	local fraction = FrameTime() * 2;
 	local target = math.Clamp(4 / dist, 2, 6);
 
 	if (!curSize) then
 		curSize = target;
-	end
+	end;
+
 	if (distance < 70) then
 		target = 10;
-	end
+	end;
+
 	if (LocalPlayer():GetVelocity():Length2D() > 150) then
 		target = 10;
-	end
+	end;
 
 	curSize = Lerp(fraction, curSize, target);
+
 	return curSize;
 end;
