df31bd0236f65e84cf443bd62ecd9221188a776e
Alex D <cod7777@yandex.ru>
2018-08-23 17:01:46 +0200

    Remove HL2RP

--- garrysmod/gamemodes/flhl2rp/plugins/flashlight/plugin/items/sh_flashlight.lua
+++
@@ -1,6 +0,0 @@
-ITEM:set_base(ItemEquippable)
-ITEM.name = "Flashlight"
-ITEM.print_name = "#Item_Flashlight_PrintName"
-ITEM.description = "#Item_Flashlight_Description"
-ITEM.model = "models/lamps/torch.mdl"
-ITEM.weight = 0.5
