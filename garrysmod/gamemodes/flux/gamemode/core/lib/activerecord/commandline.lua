0d9bcb76abafe82c40ac41a91b1e32cf41853c39
Alex D <cod7777@yandex.ru>
2018-08-26 19:52:55 +0200

    Ability to drop schema

---
+++ garrysmod/gamemodes/flux/gamemode/core/lib/activerecord/commandline.lua
@@ -0,0 +1,22 @@
+local hold_start = nil
+
+concommand.Add('ar_recreate_schema', function(player)
+  if !IsValid(player) then
+    if !hold_start or (os.time() - hold_start > 3) then
+      print(txt[[
+        ================================================
+        Warning! You are about to recreate the database!
+        This will destroy all of the data stored in it!
+        
+        Please enter this command again within 3 seconds
+                    to confirm this action.
+        ================================================
+      ]])
+      hold_start = os.time()
+    else
+      print('ActiveRecord - Wiping and recreating the database...')
+      ActiveRecord.recreate_schema()
+      hold_start = nil
+    end
+  end
+end)
