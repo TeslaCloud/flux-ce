8722f21ccd1e1d3389c29eece90580846dd40daf
Alex D <cod7777@yandex.ru>
2018-08-27 03:19:29 +0200

    Fix flux dev string

--- garrysmod/gamemodes/flux/plugins/cl_devhud.lua
+++ garrysmod/gamemodes/flux/plugins/cl_devhud.lua
@@ -5,7 +5,7 @@ PLUGIN:set_description("Adds developer HUD.")
 function PLUGIN:HUDPaint()
   if (fl.development) then
     if (hook.run("HUDPaintDeveloper") == nil) then
-      draw.SimpleText("Flux version "..(GAMEMODE.Version or "UNKNOWN")..", developer mode on.", "default", 8, ScrH() - 18, Color(200, 200, 200, 200))
+      draw.SimpleText("Flux version "..(GAMEMODE.version or "UNKNOWN")..", developer mode on.", "default", 8, ScrH() - 18, Color(200, 200, 200, 200))
     end
   end
 end
