954be5c492b045061bbbbd372bfb2d667a346519
Alex D <cod7777@yandex.ru>
2018-09-01 19:43:05 +0200

    Enums formatting

--- garrysmod/gamemodes/flux/plugins/items/plugin/sh_enums.lua
+++ garrysmod/gamemodes/flux/plugins/items/plugin/sh_enums.lua
@@ -1,3 +1,3 @@
 -- Items system
-ITEM_TEMPLATE    = -1  -- Item is a template and needs to be instantiated before use.
-ITEM_INVALID    = 0    -- Item is either invalid or is a template and therefore cannot be instantiated.
+ITEM_TEMPLATE = -1  -- Item is a template and needs to be instantiated before use.
+ITEM_INVALID  = 0   -- Item is either invalid or is a template and therefore cannot be instantiated.
