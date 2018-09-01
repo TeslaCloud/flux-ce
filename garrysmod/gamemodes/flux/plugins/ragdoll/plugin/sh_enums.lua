d0848de527cb67958f09a69db5a1a62bea091368
Alex D <cod7777@yandex.ru>
2018-09-01 19:43:05 +0200

    Enums formatting

--- garrysmod/gamemodes/flux/plugins/ragdoll/plugin/sh_enums.lua
+++ garrysmod/gamemodes/flux/plugins/ragdoll/plugin/sh_enums.lua
@@ -1,8 +1,8 @@
-ENT_RAGDOLL      = 2    -- Player's ragdoll (E.G. fallenover, death or anything else).
+ENT_RAGDOLL         = 2    -- Player's ragdoll (E.G. fallenover, death or anything else).
 
-INT_RAGDOLL_STATE  = 0    -- Player's ragdoll state (RAGDOLL_ enums).
+INT_RAGDOLL_STATE   = 0    -- Player's ragdoll state (RAGDOLL_ enums).
 
 -- Ragdoll states
-RAGDOLL_NONE    = 0    -- Not ragdolled.
+RAGDOLL_NONE        = 0    -- Not ragdolled.
 RAGDOLL_FALLENOVER  = 1    -- Ragdolled and can take damage.
-RAGDOLL_DUMMY    = 2    -- Ragdolled and cannot take damage.
+RAGDOLL_DUMMY       = 2    -- Ragdolled and cannot take damage.
