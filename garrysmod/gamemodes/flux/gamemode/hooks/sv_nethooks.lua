93cafe70190997d384e0de8eb379fe785de580b6
Alex D <cod7777@yandex.ru>
2018-09-01 19:38:51 +0200

    OCD edits

--- garrysmod/gamemodes/flux/gamemode/hooks/sv_nethooks.lua
+++ garrysmod/gamemodes/flux/gamemode/hooks/sv_nethooks.lua
@@ -3,17 +3,17 @@ netstream.Hook("ClientIncludedSchema", function(player)
   hook.run("PlayerInitialized", player)
 end)
 
-netstream.Hook("SoftUndo", function(player)
+netstream.Hook("soft_undo", function(player)
   fl.undo:DoPlayer(player)
 end)
 
-netstream.Hook("LocalPlayerCreated", function(player)
-  netstream.Start(player, "SharedTables", fl.shared)
+netstream.Hook("player_created", function(player)
+  netstream.Heavy(player, "SharedTables", fl.shared)
 
   player:SendConfig()
   player:SyncNetVars()
 end)
 
-netstream.Hook("Flux::Player::Language", function(player, lang)
+netstream.Hook("player_set_lang", function(player, lang)
   player:set_nv("language", lang)
 end)
