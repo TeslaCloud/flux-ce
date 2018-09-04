b6c5cd23cd13c36d0672e2eb3cd885c3b43cedf4
Alex D <cod7777@yandex.ru>
2018-09-04 02:32:27 +0200

    Update dev hud

--- garrysmod/gamemodes/flux/plugins/cl_devhud.lua
+++ garrysmod/gamemodes/flux/plugins/cl_devhud.lua
@@ -5,7 +5,7 @@ PLUGIN:set_description("Adds developer HUD.")
 function PLUGIN:HUDPaint()
   if (fl.development) then
     if (hook.run("HUDPaintDeveloper") == nil) then
-      draw.SimpleText("Flux version "..(GAMEMODE.version or "UNKNOWN")..", developer mode on.", "default", 8, ScrH() - 18, Color(200, 200, 200, 200))
+      draw.SimpleText("Flux version "..(GAMEMODE.version or "UNKNOWN")..", development environment.", "default", 8, ScrH() - 18, Color(200, 100, 100, 200))
     end
   end
 end
