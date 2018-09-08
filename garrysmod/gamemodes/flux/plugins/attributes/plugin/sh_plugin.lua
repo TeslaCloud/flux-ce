4472e2d217bc463c2e502b2dd420b8e6e7e3e618
xAleXXX007x <AleXXX_007@inbox.ru>
2018-09-08 16:32:57 +0400

    [Attributes] Default types removed

--- garrysmod/gamemodes/flux/plugins/attributes/plugin/sh_plugin.lua
+++ garrysmod/gamemodes/flux/plugins/attributes/plugin/sh_plugin.lua
@@ -2,14 +2,6 @@ PLUGIN:set_alias("flAttributes")
 
 util.include("sv_hooks.lua")
 
-function flAttributes:OnPluginLoaded()
-  local dir = self:get_folder()
-
-  attributes.register_type("skills", "SKILL", dir.."skills/")
-  attributes.register_type("stats", "STAT", dir.."stats/")
-  attributes.register_type("perks", "PERK", dir.."perks/")
-end
-
 function flAttributes:PluginIncludeFolder(extra, folder)
   for k, v in pairs(attributes.types) do
     if extra == k then
