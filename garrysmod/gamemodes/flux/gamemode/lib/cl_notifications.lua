26728c90334c85c19d9bdd9268a98c96fe0aa75d
Alex D <cod7777@yandex.ru>
2018-09-16 00:29:16 +0200

    Fix notification overlap

--- garrysmod/gamemodes/flux/gamemode/lib/cl_notifications.lua
+++ garrysmod/gamemodes/flux/gamemode/lib/cl_notifications.lua
@@ -2,49 +2,66 @@ library.new("notification", fl)
 
 local display = {}
 local top = 1
-local lastReposition = CurTime()
+local queue = {}
+local queue_locked = false
 
-function fl.notification:Add(text, lifetime, textColor, backColor)
-  local scrW = ScrW()
-  lifetime = lifetime or 8
-  text = t(text) or ""
+function fl.notification:process_queue()
+  local notification = queue[1]
 
-  display[top] = {text = text, lifetime = lifetime, panel = nil, width = 0, height = 0, isLast = true}
+  if !queue_locked and notification then
+    queue_locked = true
 
-  if display[top - 1] then
-    display[top - 1].isLast = false
+    local text, lifetime = notification.text, notification.lifetime
+    local scrW = ScrW()
+    lifetime = lifetime or 8
+    text = t(text) or ""
+
+    display[top] = {text = text, lifetime = lifetime, panel = nil, width = 0, height = 0, isLast = true}
+
+    if display[top - 1] then
+      display[top - 1].isLast = false
+    end
+
+    local panel = vgui.Create("fl_notification")
+    panel:SetText(text)
+    panel:SetLifetime(lifetime)
+    panel:SetTextColor(notification.text_color)
+    panel:SetBackgroundColor(notification.back_color)
+
+    local w, h = panel:GetSize()
+    panel:SetPos(scrW - w - 8, -h)
+    panel:MoveTo(scrW - w - 8, 8, 0.13)
+
+    display[top].panel = panel
+    display[top].width = w
+    display[top].height = h
+
+    timer.Simple(lifetime, function()
+      display[top] = nil
+    end)
+
+    top = top + 1
+
+    self:Reposition(h)
+
+    table.remove(queue, 1)
+
+    timer.Simple(0.3, function() queue_locked = false fl.notification:process_queue() end)
   end
-
-  local panel = vgui.Create("fl_notification")
-  panel:SetText(text)
-  panel:SetLifetime(lifetime)
-  panel:SetTextColor(textColor)
-  panel:SetBackgroundColor(backColor)
-
-  local w, h = panel:GetSize()
-  panel:SetPos(scrW - w - 8, -h)
-  panel:MoveTo(scrW - w - 8, 8, 0.13)
-
-  display[top].panel = panel
-  display[top].width = w
-  display[top].height = h
-
-  timer.Simple(lifetime, function()
-    display[top] = nil
-  end)
-
-  top = top + 1
-
-  self:Reposition(h)
 end
 
-function fl.notification:AddPopup(text, lifetime, x, y, textColor, backColor)
+function fl.notification:Add(text, lifetime, text_color, back_color)
+  table.insert(queue, { text = text, lifetime = lifetime, text_color = text_color, back_color = back_color })
+  self:process_queue()
+end
+
+function fl.notification:AddPopup(text, lifetime, x, y, text_color, back_color)
   local panel = vgui.Create("fl_notification")
   panel:SetPos(x, y)
   panel:SetText(text)
   panel:SetLifetime(lifetime)
-  panel:SetTextColor(textColor)
-  panel:SetBackgroundColor(backColor)
+  panel:SetTextColor(text_color)
+  panel:SetBackgroundColor(back_color)
 
   function panel:PostThink()
     self:MoveToFront()
@@ -54,21 +71,11 @@ end
 function fl.notification:Reposition(offset)
   if !isnumber(offset) then return end
 
-  local curTime = CurTime()
+  for k, v in ipairs(display) do
+    if v and IsValid(v.panel) then
+      local x, y = v.panel:GetPos()
 
-  if lastReposition + 0.3 < curTime then
-    for k, v in ipairs(display) do
-      if v and IsValid(v.panel) then
-        local x, y = v.panel:GetPos()
-
-        v.panel:MoveTo(x, y + offset + 4, 0.13)
-      end
+      v.panel:MoveTo(x, y + offset + 4, 0.13)
     end
-
-    lastReposition = curTime
-  else
-    timer.Simple(0.3 - (curTime - lastReposition), function()
-      self:Reposition()
-    end)
   end
 end
