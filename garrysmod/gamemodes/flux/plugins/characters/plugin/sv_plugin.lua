de8e44f9030affda6fd4cdccf85b401bdef75b2b
Alex D <cod7777@yandex.ru>
2018-09-15 12:05:21 +0200

    Fix error in characters

--- garrysmod/gamemodes/flux/plugins/characters/plugin/sv_plugin.lua
+++ garrysmod/gamemodes/flux/plugins/characters/plugin/sv_plugin.lua
@@ -8,7 +8,7 @@ function player_meta:SetActiveCharacter(id)
   end
 
   self:set_nv('active_character', tonumber(id))
-  self.current_character = player.record.characters[tonumber(id)]
+  self.current_character = self.record.characters[tonumber(id)]
 
   local char_data = self:GetCharacter()
 
