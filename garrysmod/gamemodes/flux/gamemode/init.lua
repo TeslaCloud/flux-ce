7306cdea374a065d657fa48b073bd205a9922d35
Alex D <cod7777@yandex.ru>
2018-09-21 19:16:19 +0200

    Do less stuff on reload

--- garrysmod/gamemodes/flux/gamemode/init.lua
+++ garrysmod/gamemodes/flux/gamemode/init.lua
@@ -20,33 +20,35 @@ if fl.initialized then
   end
 end
 
-if !safe_require 'fileio' then
-  ErrorNoHalt('The fileio module has failed to load!\nPlease make sure that you have gmsv_fileio_'..((system.IsWindows() and 'win32') or 'linux')..'.dll in garrysmod/lua/bin folder!\nAborting startup...\n')
-  return
-end
+if !LITE_REFRESH then
+  if !safe_require 'fileio' then
+    ErrorNoHalt('The fileio module has failed to load!\nPlease make sure that you have gmsv_fileio_'..((system.IsWindows() and 'win32') or 'linux')..'.dll in garrysmod/lua/bin folder!\nAborting startup...\n')
+    return
+  end
 
--- Put that under an if because it doesn't change.
-if !string.utf8len or !pon or !netstream then
-  AddCSLuaFile 'thirdparty/utf8.lua'
-  AddCSLuaFile 'thirdparty/pon.lua'
-  AddCSLuaFile 'thirdparty/netstream.lua'
-end
+  -- Put that under an if because it doesn't change.
+  if !string.utf8len or !pon or !netstream then
+    AddCSLuaFile 'thirdparty/utf8.lua'
+    AddCSLuaFile 'thirdparty/pon.lua'
+    AddCSLuaFile 'thirdparty/netstream.lua'
+  end
 
-AddCSLuaFile 'shared.lua'
+  AddCSLuaFile 'shared.lua'
 
--- Include pON, Netstream and UTF-8 library.
-if !string.utf8len or !pon or !netstream or !YAML then
-  include 'thirdparty/utf8.lua'
-  include 'thirdparty/pon.lua'
-  include 'thirdparty/netstream.lua'
-  YAML = include 'thirdparty/yaml.lua'
+  -- Include pON, Netstream and UTF-8 library.
+  if !string.utf8len or !pon or !netstream or !YAML then
+    include 'thirdparty/utf8.lua'
+    include 'thirdparty/pon.lua'
+    include 'thirdparty/netstream.lua'
+    YAML = include 'thirdparty/yaml.lua'
 
-  Settings = Settings or YAML.read('gamemodes/flux/config/settings.yml')
-  Settings.database = YAML.read('gamemodes/flux/config/database.yml')
+    Settings = Settings or YAML.read('gamemodes/flux/config/settings.yml')
+    Settings.database = YAML.read('gamemodes/flux/config/database.yml')
+  end
 end
 
 -- Initiate shared boot.
-include('shared.lua')
+include 'shared.lua'
 
 if fl.initialized then
   MsgC(Color(0, 255, 100, 255), 'Auto-reloaded in '..math.Round(os.clock() - fl.start_time, 3).. ' second(s)\n')
