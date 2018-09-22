10f4b49e1df9e8ad2342ec7c4fb0cc6f8572f25a
xAleXXX007x <AleXXX_007@inbox.ru>
2018-09-22 11:52:53 +0400

    [UI] sidebar:add_button() returned

--- garrysmod/gamemodes/flux/gamemode/views/base/cl_sidebar.lua
+++ garrysmod/gamemodes/flux/gamemode/views/base/cl_sidebar.lua
@@ -44,6 +44,31 @@ function PANEL:SetMargin(margin)
   self.margin = tonumber(margin) or 0
 end
 
+function PANEL:add_button(text, callback)
+  local button = vgui.Create('fl_button', self)
+  button:SetSize(self:GetWide(), theme.GetOption('menu_sidebar_height'))
+  button:SetText(text)
+  button:SetDrawBackground(true)
+  button:SetFont(theme.GetFont('text_normal_smaller'))
+  button:SetTextAutoposition(true)
+  button.DoClick = function(btn)
+    btn:SetActive(true)
+
+    if IsValid(self.prevButton) and self.prevButton != btn then
+      self.prevButton:SetActive(false)
+    end
+     self.prevButton = btn
+
+    if isfunction(callback) then
+      callback(btn)
+    end
+  end
+
+  self:AddPanel(button)
+
+  return button
+end
+
 -- 'borrowed' from lua/vgui/dscrollpanel.lua
 function PANEL:PerformLayout()
   local oldHeight = self.pnlCanvas:GetTall()
