3a5bb38ed7efb274e450e348e4fae0959a32e3be
xAleXXX007x <AleXXX_007@inbox.ru>
2018-09-23 21:00:45 +0400

    [Attributes] Fixes and reorganization

--- garrysmod/gamemodes/flux/plugins/attributes/plugin/sv_hooks.lua
+++ garrysmod/gamemodes/flux/plugins/attributes/plugin/sv_hooks.lua
@@ -1,5 +1,11 @@
-function flAttributes:PostCreateCharacter(player, charID, char)
-  char.attributes = char.attributes or {}
+function flAttributes:PostCreateCharacter(player, char_id, char, char_data)
+  for k, v in pairs(attributes.get_stored()) do
+    local atts_table = {}
+
+    atts_table[k] = {}
+  end
+
+  char.attributes = atts_table
 end
 
 function flAttributes:OnActiveCharacterSet(player, character)
