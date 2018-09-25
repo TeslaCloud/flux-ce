f573649f32e9ad1d6939e5b5060daccf2d45bd7e
xAleXXX007x <AleXXX_007@inbox.ru>
2018-09-25 22:13:56 +0400

    [Attributes] Raw boosts and multipliers

--- garrysmod/gamemodes/flux/plugins/attributes/plugin/migrations/CreateAttributes.lua
+++ garrysmod/gamemodes/flux/plugins/attributes/plugin/migrations/CreateAttributes.lua
@@ -3,3 +3,15 @@ ActiveRecord.define_model('attributes', function(t)
   t:integer 'character_id'
   t:integer 'value'
 end)
+
+ActiveRecord.define_model('attribute_multipliers', function(t)
+  t:integer 'attribute_id'
+  t:integer 'value'
+  t:integer 'duration'
+end)
+
+ActiveRecord.define_model('attribute_boosts', function(t)
+  t:integer 'attribute_id'
+  t:integer 'value'
+  t:integer 'duration'
+end)
