85662c8535a272d133256c4d8c43838887f3113e
Alex D <cod7777@yandex.ru>
2018-10-01 21:40:35 +0200

    Add 3D pictures

--- garrysmod/gamemodes/flux/plugins/3dtexts/plugin/cl_hooks.lua
+++ garrysmod/gamemodes/flux/plugins/3dtexts/plugin/cl_hooks.lua
@@ -1,54 +1,85 @@
 local blurTexture = Material('pp/blurscreen')
+local color_white = Color(255, 255, 255)
+
+function fl3DText:DrawTextPreview()
+  local tool = fl.client:GetTool()
+  local text = tool:GetClientInfo('text')
+  local style = tool:GetClientNumber('style')
+  local trace = fl.client:GetEyeTrace()
+  local normal = trace.HitNormal
+  local w, h = util.text_size(text, theme.get_font('text_3d2d'))
+  local angle = normal:Angle()
+  angle:RotateAroundAxis(angle:Forward(), 90)
+  angle:RotateAroundAxis(angle:Right(), 270)
+
+  cam.Start3D2D(trace.HitPos + (normal * 1.25), angle, 0.1 * tool:GetClientNumber('scale'))
+    if style >= 5 then
+      if style != 8 and style != 9 then
+        draw.RoundedBox(0, -w * 0.5 - 32, -h * 0.5 - 16, w + 64, h + 32, Color(tool:GetClientNumber('r2', 0), tool:GetClientNumber('g2', 0), tool:GetClientNumber('b2', 0), 40))
+      end
+
+      if style == 7 or style == 8 then
+        draw.RoundedBox(0, -w * 0.5 - 32, -h * 0.5 - 16, w + 64, 6, Color(255, 255, 255, 40))
+        draw.RoundedBox(0, -w * 0.5 - 32, -h * 0.5 + h + 10, w + 64, 6, Color(255, 255, 255, 40))
+      elseif style == 9 then
+        local wide = w + 64
+        local barColor = Color(255, 255, 255, 40)
+        local barX, barY = -w * 0.5 - 32, -h * 0.5 - 16
+        local rectWidth = (wide / 3 - wide / 6) * 0.75
+
+        -- Draw left thick rectangles
+        draw.RoundedBox(0, barX, barY - 6, rectWidth, 10, barColor)
+        draw.RoundedBox(0, barX, barY + h + 22, rectWidth, 10, barColor)
+
+        -- ...and the right ones
+        draw.RoundedBox(0, barX + wide - rectWidth, barY - 6, rectWidth, 10, barColor)
+        draw.RoundedBox(0, barX + wide - rectWidth, barY + h + 22, rectWidth, 10, barColor)
+
+        -- And the middle thingies
+        draw.RoundedBox(0, -(wide / 1.75) * 0.5, barY, wide / 1.75, 4, barColor)
+        draw.RoundedBox(0, -(wide / 1.75) * 0.5, barY + h + 22, wide / 1.75, 4, barColor)
+      end
+    end
+
+    draw.SimpleText(text, theme.get_font('text_3d2d'), -w * 0.5, -h * 0.5, Color(tool:GetClientNumber('r', 0), tool:GetClientNumber('g', 0), tool:GetClientNumber('b', 0), 60))
+  cam.End3D2D()
+end
+
+function fl3DText:DrawPicturePreview()
+  local tool = fl.client:GetTool()
+  local url = tool:GetClientInfo('url')
+  local width = tool:GetClientNumber('width')
+  local height = tool:GetClientNumber('height')
+  local trace = fl.client:GetEyeTrace()
+  local normal = trace.HitNormal
+  local angle = normal:Angle()
+  angle:RotateAroundAxis(angle:Forward(), 90)
+  angle:RotateAroundAxis(angle:Right(), 270)
+
+  cam.Start3D2D(trace.HitPos + (normal * 1.25), angle, 0.1)
+    if url:ends('.png') or url:ends('.jpg') or url:ends('.jpeg') then
+      draw.textured_rect(URLMaterial(url), -width * 0.5, -height * 0.5, width, height, color_white)
+    else
+      draw.RoundedBox(0, -width * 0.5, -height * 0.5, width, height, Color(255, 0, 0, 40))      
+    end
+  cam.End3D2D()
+end
 
 function fl3DText:PostDrawOpaqueRenderables()
   local weapon = fl.client:GetActiveWeapon()
   local clientPos = fl.client:GetPos()
 
-  if IsValid(weapon) and weapon:GetClass() == 'gmod_tool' and weapon:GetMode() == 'texts' then
-    local tool = fl.client:GetTool()
-    local text = tool:GetClientInfo('text')
-    local style = tool:GetClientNumber('style')
-    local trace = fl.client:GetEyeTrace()
-    local normal = trace.HitNormal
-    local w, h = util.text_size(text, theme.get_font('text_3d2d'))
-    local angle = normal:Angle()
-    angle:RotateAroundAxis(angle:Forward(), 90)
-    angle:RotateAroundAxis(angle:Right(), 270)
+  if IsValid(weapon) and weapon:GetClass() == 'gmod_tool' then
+    local mode = weapon:GetMode()
 
-    cam.Start3D2D(trace.HitPos + (normal * 1.25), angle, 0.1 * tool:GetClientNumber('scale'))
-      if style >= 5 then
-        if style != 8 and style != 9 then
-          draw.RoundedBox(0, -w * 0.5 - 32, -h * 0.5 - 16, w + 64, h + 32, Color(tool:GetClientNumber('r2', 0), tool:GetClientNumber('g2', 0), tool:GetClientNumber('b2', 0), 40))
-        end
-
-        if style == 7 or style == 8 then
-          draw.RoundedBox(0, -w * 0.5 - 32, -h * 0.5 - 16, w + 64, 6, Color(255, 255, 255, 40))
-          draw.RoundedBox(0, -w * 0.5 - 32, -h * 0.5 + h + 10, w + 64, 6, Color(255, 255, 255, 40))
-        elseif style == 9 then
-          local wide = w + 64
-          local barColor = Color(255, 255, 255, 40)
-          local barX, barY = -w * 0.5 - 32, -h * 0.5 - 16
-          local rectWidth = (wide / 3 - wide / 6) * 0.75
-
-          -- Draw left thick rectangles
-          draw.RoundedBox(0, barX, barY - 6, rectWidth, 10, barColor)
-          draw.RoundedBox(0, barX, barY + h + 22, rectWidth, 10, barColor)
-
-          -- ...and the right ones
-          draw.RoundedBox(0, barX + wide - rectWidth, barY - 6, rectWidth, 10, barColor)
-          draw.RoundedBox(0, barX + wide - rectWidth, barY + h + 22, rectWidth, 10, barColor)
-
-          -- And the middle thingies
-          draw.RoundedBox(0, -(wide / 1.75) * 0.5, barY, wide / 1.75, 4, barColor)
-          draw.RoundedBox(0, -(wide / 1.75) * 0.5, barY + h + 22, wide / 1.75, 4, barColor)
-        end
-      end
-
-      draw.SimpleText(text, theme.get_font('text_3d2d'), -w * 0.5, -h * 0.5, Color(tool:GetClientNumber('r', 0), tool:GetClientNumber('g', 0), tool:GetClientNumber('b', 0), 60))
-    cam.End3D2D()
+    if mode == 'texts' then
+      self:DrawTextPreview()
+    elseif mode == 'pictures' then
+      self:DrawPicturePreview()
+    end
   end
 
-  for k, v in ipairs(self.stored) do
+  for k, v in ipairs(self.texts) do
     local pos = v.pos
     local distance = clientPos:Distance(pos)
     local fadeOffset = v.fadeOffset or 1000
@@ -102,7 +133,7 @@ function fl3DText:PostDrawOpaqueRenderables()
 
             render.SetMaterial(blurTexture)
 
-            for i = 0, 1, 0.2 do
+            for i = 0, 1, 0.3 do
               blurTexture:SetFloat('$blur', i * 8)
               blurTexture:Recompute()
               render.UpdateScreenEffectTexture()
@@ -155,6 +186,30 @@ function fl3DText:PostDrawOpaqueRenderables()
 
     cam.Start3D2D(pos + (normal * 1.25 * (scale + 0.5)), angle, 0.1 * scale)
       draw.SimpleText(text, theme.get_font('text_3d2d'), posX, posY, ColorAlpha(textColor, fadeAlpha))
+    cam.End3D2D()
+  end
+
+  for k, v in ipairs(self.pictures) do
+    local pos = v.pos
+    local distance = clientPos:Distance(pos)
+    local fade_offset = v.fade_offset or 1000
+    local draw_distance = 1024 + fade_offset
+
+    if distance > draw_distance then continue end
+
+    local fade_alpha = 255
+    local fade_distance = 768 + fade_offset
+
+    if distance > fade_distance then
+      local d = distance - fade_distance
+      fade_alpha = math.Clamp((255 * ((draw_distance - fade_distance) - d) / (draw_distance - fade_distance)), 0, 255)
+    end
+
+    local height = v.height
+    local width = v.width
+
+    cam.Start3D2D(pos + (v.normal * 0.4), v.angle, 0.1)
+      draw.textured_rect(URLMaterial(v.url), -width * 0.5, -height * 0.5, width, height, ColorAlpha(color_white, fade_alpha))
     cam.End3D2D()
   end
 end
--- garrysmod/gamemodes/flux/plugins/3dtexts/plugin/sh_plugin.lua
+++ garrysmod/gamemodes/flux/plugins/3dtexts/plugin/sh_plugin.lua
@@ -1,22 +1,27 @@
 PLUGIN:set_alias('fl3DText')
 
-fl3DText.stored = fl3DText.stored or {}
+fl3DText.texts = fl3DText.texts or {}
+fl3DText.pictures = fl3DText.pictures or {}
 
 util.include('cl_hooks.lua')
 
 if SERVER then
   function fl3DText:Save()
-    data.SavePlugin('3dtexts', fl3DText.stored)
+    data.SavePlugin('3dtexts', fl3DText.texts)
+    data.SavePlugin('3dpictures', fl3DText.pictures)
   end
 
   function fl3DText:Load()
     local loaded = data.LoadPlugin('3dtexts', {})
+    local loaded_pics = data.LoadPlugin('3dpictures', {})
 
-    self.stored = loaded
+    self.texts = loaded
+    self.pictures = loaded_pics
   end
 
   function fl3DText:PlayerInitialized(player)
-    netstream.Start(player, 'flLoad3DTexts', self.stored)
+    netstream.Start(player, 'flLoad3DTexts', self.texts)
+    netstream.Start(player, 'flLoad3DPictures', self.pictures)
   end
 
   function fl3DText:LoadData()
@@ -30,11 +35,21 @@ if SERVER then
   function fl3DText:AddText(data)
     if !data or !data.text or !data.pos or !data.angle or !data.style or !data.scale then return end
 
-    table.insert(fl3DText.stored, data)
+    table.insert(fl3DText.texts, data)
 
     self:Save()
 
     netstream.Start(nil, 'fl3DText_Add', data)
+  end
+
+  function fl3DText:AddPicture(data)
+    if !data or !data.url or !data.width or !data.height then return end
+
+    table.insert(fl3DText.pictures, data)
+
+    self:Save()
+
+    netstream.Start(nil, 'fl3DPicture_Add', data)
   end
 
   function fl3DText:Remove(player)
@@ -43,9 +58,15 @@ if SERVER then
     end
   end
 
+  function fl3DText:RemovePicture(player)
+    if player:can('textremove') then
+      netstream.Start(player, 'fl3DPicture_Calculate', true)
+    end
+  end
+
   netstream.Hook('fl3DText_Remove', function(player, idx)
     if player:can('textremove') then
-      table.remove(fl3DText.stored, idx)
+      table.remove(fl3DText.texts, idx)
 
       fl3DText:Save()
 
@@ -54,21 +75,49 @@ if SERVER then
       fl.player:notify(player, t'3d_text.text_removed')
     end
   end)
+
+  netstream.Hook('fl3DPicture_Remove', function(player, idx)
+    if player:can('textremove') then
+      table.remove(fl3DText.pictures, idx)
+
+      fl3DText:Save()
+
+      netstream.Start(nil, 'fl3DPicture_Remove', idx)
+
+      fl.player:notify(player, t'3d_picture.removed')
+    end
+  end)
 else
   netstream.Hook('flLoad3DTexts', function(data)
-    fl3DText.stored = data or {}
+    fl3DText.texts = data or {}
   end)
 
   netstream.Hook('fl3DText_Add', function(data)
-    table.insert(fl3DText.stored, data)
+    table.insert(fl3DText.texts, data)
   end)
 
   netstream.Hook('fl3DText_Remove', function(idx)
-    table.remove(fl3DText.stored, idx)
+    table.remove(fl3DText.texts, idx)
+  end)
+
+  netstream.Hook('flLoad3DPictures', function(data)
+    fl3DText.pictures = data or {}
+  end)
+
+  netstream.Hook('fl3DPicture_Add', function(data)
+    table.insert(fl3DText.pictures, data)
+  end)
+
+  netstream.Hook('fl3DPicture_Remove', function(idx)
+    table.remove(fl3DText.pictures, idx)
   end)
 
   netstream.Hook('fl3DText_Calculate', function()
     fl3DText:RemoveAtTrace(fl.client:GetEyeTraceNoCursor())
+  end)
+
+  netstream.Hook('fl3DPicture_Calculate', function()
+    fl3DText:RemovePictureAtTrace(fl.client:GetEyeTraceNoCursor())
   end)
 
   function fl3DText:RemoveAtTrace(trace)
@@ -77,7 +126,7 @@ else
     local hit_pos = trace.HitPos - trace.HitNormal
     local trace_start = trace.StartPos
 
-    for k, v in pairs(self.stored) do
+    for k, v in pairs(self.texts) do
       local pos = v.pos
       local normal = v.normal
       local ang = normal:Angle()
@@ -89,6 +138,33 @@ else
       if math.abs(math.abs(hit_pos.z) - math.abs(pos.z)) < 4 * v.scale then
         if util.vectors_intersect(trace_start, hit_pos, start_pos, end_pos) then
           netstream.Start('fl3DText_Remove', k)
+
+          return true
+        end
+      end
+    end
+
+    return false
+  end
+
+  function fl3DText:RemovePictureAtTrace(trace)
+    if !trace then return false end
+
+    local hit_pos = trace.HitPos - trace.HitNormal
+    local trace_start = trace.StartPos
+
+    for k, v in pairs(self.pictures) do
+      local pos = v.pos
+      local normal = v.normal
+      local ang = normal:Angle()
+      local width, height = v.width, v.height
+      local ang_right = -ang:Right()
+      local start_pos = pos - ang_right * (width * 0.05)
+      local end_pos = pos + ang_right * (width * 0.05)
+
+      if math.abs(math.abs(hit_pos.z) - math.abs(pos.z)) < height * 0.05 then
+        if util.vectors_intersect(trace_start, hit_pos, start_pos, end_pos) then
+          netstream.Start('fl3DPicture_Remove', k)
 
           return true
         end
