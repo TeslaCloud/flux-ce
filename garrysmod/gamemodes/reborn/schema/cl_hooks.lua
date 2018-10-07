fd428b5efdae1d770b7f6b2633369cb012012891
Alex D <cod7777@yandex.ru>
2018-10-07 02:51:58 +0200

    Fix reborn

--- garrysmod/gamemodes/reborn/schema/cl_hooks.lua
+++ garrysmod/gamemodes/reborn/schema/cl_hooks.lua
@@ -1,4 +1,4 @@
 -- Clientside hooks would go here.
 function Schema:HUDPaint()
-  draw.SimpleText(t'reborn.welcome_text', 'DermaLarge', 16, 16, theme.Hook('TestHook'))
+  draw.SimpleText(t'reborn.welcome_text', 'DermaLarge', 16, 16, theme.hook('TestHook'))
 end
