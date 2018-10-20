e6b37cef22e98d7ea96e642cba43d447cdfcc043
Alex D <cod7777@yandex.ru>
2018-10-20 04:32:54 +0200

    Let AR handle relations, seriously, it's better at it

--- garrysmod/gamemodes/flux/plugins/attributes/plugin/sv_hooks.lua
+++ garrysmod/gamemodes/flux/plugins/attributes/plugin/sv_hooks.lua
@@ -1,30 +1,19 @@
 function flAttributes:PostCreateCharacter(player, char_id, char, char_data)
   char.attributes = {}
-  char.attribute_multipliers = {}
-  char.attribute_boosts = {}
 
   for k, v in pairs(attributes.get_stored()) do
     local attribute = attributes.find_by_id(k)
 
     local att = Attribute.new()
-    att.character_id = char.id
-    att.attr_id = k
-    att.value = char_data.attributes[k] or attribute.min
-
+      att.character_id = char.id
+      att.attr_id = k
+      att.value = char_data.attributes[k] or attribute.min
     table.insert(char.attributes, att)
-
-    att:save()
   end
 end
 
 function flAttributes:OnCharacterDelete(player, char_id)
   for k, v in pairs(player.record.characters[char_id].attributes) do
     v:destroy()
-  end
-end
-
-function flAttributes:SaveCharacterData(player, char)
-  for k, v in pairs(player.record.characters[char.id].attributes) do
-    v:save()
   end
 end
