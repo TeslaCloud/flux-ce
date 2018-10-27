a1741b987d57a7dd5489238a23ee5928a46c4319
Alex D <cod7777@yandex.ru>
2018-10-28 00:48:20 +0200

    Sneaky tabs!

--- garrysmod/gamemodes/flux/gamemode/lib/meta/cl_panel.lua
+++ garrysmod/gamemodes/flux/gamemode/lib/meta/cl_panel.lua
@@ -23,45 +23,45 @@ local model_panel = vgui.GetControlTable('DModelPanel')
 function model_panel:Paint(w, h)
   local ent = self.Entity
 
-	if !IsValid(ent) then return end
+  if !IsValid(ent) then return end
 
-	local x, y = self:LocalToScreen(0, 0)
+  local x, y = self:LocalToScreen(0, 0)
 
-	self:LayoutEntity(ent)
+  self:LayoutEntity(ent)
 
-	local ang = self.aLookAngle
+  local ang = self.aLookAngle
 
-	if !ang then
-		ang = (self.vLookatPos - self.vCamPos):Angle()
-	end
+  if !ang then
+    ang = (self.vLookatPos - self.vCamPos):Angle()
+  end
 
-	cam.Start3D(self.vCamPos, ang, self.fFOV, x, y, w, h, 5, self.FarZ)
+  cam.Start3D(self.vCamPos, ang, self.fFOV, x, y, w, h, 5, self.FarZ)
 
   -- Fix for models being behind blur texture in Z-buffer.
   if fl.should_render_blur then cam.IgnoreZ(true) end
 
-	render.SuppressEngineLighting(true)
-	render.SetLightingOrigin(ent:GetPos())
-	render.ResetModelLighting(self.colAmbientLight.r / 255, self.colAmbientLight.g / 255, self.colAmbientLight.b / 255)
-	render.SetColorModulation(self.colColor.r / 255, self.colColor.g / 255, self.colColor.b / 255)
-	render.SetBlend((self:GetAlpha() / 255) * (self.colColor.a / 255))
+  render.SuppressEngineLighting(true)
+  render.SetLightingOrigin(ent:GetPos())
+  render.ResetModelLighting(self.colAmbientLight.r / 255, self.colAmbientLight.g / 255, self.colAmbientLight.b / 255)
+  render.SetColorModulation(self.colColor.r / 255, self.colColor.g / 255, self.colColor.b / 255)
+  render.SetBlend((self:GetAlpha() / 255) * (self.colColor.a / 255))
 
-	for i = 0, 6 do
-		local col = self.DirectionalLight[i]
+  for i = 0, 6 do
+    local col = self.DirectionalLight[i]
 
-		if col then
-			render.SetModelLighting(i, col.r / 255, col.g / 255, col.b / 255)
-		end
-	end
+    if col then
+      render.SetModelLighting(i, col.r / 255, col.g / 255, col.b / 255)
+    end
+  end
 
-	self:DrawModel()
+  self:DrawModel()
 
-	render.SuppressEngineLighting(false)
+  render.SuppressEngineLighting(false)
 
   -- End fix
   if fl.should_render_blur then cam.IgnoreZ(false) end
 
-	cam.End3D()
+  cam.End3D()
 
-	self.LastPaint = RealTime()
+  self.LastPaint = RealTime()
 end
