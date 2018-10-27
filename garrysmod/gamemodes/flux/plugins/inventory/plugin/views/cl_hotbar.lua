db051d4363b21f53e220232c5b1c8abdb734024a
Alex D <cod7777@yandex.ru>
2018-10-28 01:52:00 +0200

    Fix hotbar

--- garrysmod/gamemodes/flux/plugins/inventory/plugin/views/cl_hotbar.lua
+++ garrysmod/gamemodes/flux/plugins/inventory/plugin/views/cl_hotbar.lua
@@ -3,18 +3,22 @@ PANEL.inventory_slots = 8
 PANEL.inventory_type = 'hotbar'
 PANEL.draw_inventory_slots = true
 
+function PANEL:Rebuild()
+  local w, h = self:GetSize()
+  local cx, cy = ScrC()
+  self:SetPos(cx - w * 0.5, ScrH() - h - font.Scale(32))
+
+  self.BaseClass.Rebuild(self)
+end
+
 vgui.Register('fl_hotbar', PANEL, 'fl_inventory')
 
 concommand.Add('fl_hotbar_rebuild', function()
   if IsValid(fl.client.hotbar) then
     local hotbar = fl.client.hotbar
-    hotbar:SetVisible(false)
     hotbar:Remove()
     hotbar = fl_inventory:create_hotbar()
     hotbar:Rebuild()
-    local w, h = hotbar:GetSize()
-    local cx, cy = ScrC()
-    hotbar:SetVisible(true)
-    hotbar:SetPos(cx - w * 0.5, ScrH() - h - font.Scale(32))
+    hotbar:SetVisible(false)
   end
 end)
