b8cd8468166eb41fb2972042125460a04b721ccc
AleXXX_007 <AleXXX_007@inbox.ru>
2019-02-02 17:54:50 +0400

    [Bolt] Permissions registration hook

--- garrysmod/gamemodes/flux/plugins/observer/plugin/sh_plugin.lua
+++ garrysmod/gamemodes/flux/plugins/observer/plugin/sh_plugin.lua
@@ -3,12 +3,12 @@ PLUGIN:set_global('Observer')
 util.include('cl_hooks.lua')
 util.include('sv_hooks.lua')
 
-if fl.admin then
-  Bolt:register_permission('noclip', 'Noclip', 'Lets the player use observer mode / noclip.', 'general')
-end
-
 if SERVER then
   config.set('observer_reset', false)
 else
   config.add_to_menu('observer_reset', 'Observer Reset', "Whether or not should player's position be restored when they leave observer mode?", 'boolean')
+end
+
+function Bolt:RegisterPermissions()
+  Bolt:register_permission('noclip', 'Noclip', 'Lets the player use observer mode / noclip.', 'categories.general')
 end
