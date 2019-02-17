afc6b66a57a7366abdd8d29b5305bf6590dd466a
Alex D <cod7777@yandex.ru>
2019-02-17 21:19:19 +0100

    Nicer crosshair

--- garrysmod/gamemodes/flux/plugins/cl_crosshair.lua
+++ garrysmod/gamemodes/flux/plugins/cl_crosshair.lua
@@ -6,14 +6,22 @@ PLUGIN:set_description('Adds a crosshair.')
 
 local size = 2
 local half_size = size * 0.5
+local double_size = size * 2
 local gap = 8
 local cur_gap = gap
 
+function PLUGIN:ShouldHUDPaintCrosshair()
+  if fl.client:running() or !fl.client:Alive() or !fl.client:has_initialized() then
+    return false
+  end
+end
+
 function PLUGIN:HUDPaint()
-  if hook.run('ShouldHUDPaint') != false then
+  if hook.run('ShouldHUDPaint') != false and hook.run('ShouldHUDPaintCrosshair') != false then
     local trace = fl.client:GetEyeTraceNoCursor()
     local distance = fl.client:GetPos():Distance(trace.HitPos)
-    local draw_color = plugin.call('AdjustCrosshairColor', trace, distance) or Color(255, 255, 255)
+    local draw_color = plugin.call('AdjustCrosshairColor', trace, distance) or color_white
+    local secondary_draw_color = draw_color:alpha(25)
     local real_gap = plugin.call('AdjustCrosshairGap', trace, distance) or math.Round(gap * math.Clamp(distance / 400, 0.5, 4))
     cur_gap = Lerp(FrameTime() * 6, cur_gap, real_gap)
 
@@ -21,15 +29,19 @@ function PLUGIN:HUDPaint()
       cur_gap = real_gap
     end
 
+    if draw_color != color_white then
+      secondary_draw_color = secondary_draw_color:alpha(255)
+    end
+
     local scrw, scrh = ScrW(), ScrH()
 
     draw.RoundedBox(0, scrw * 0.5 - half_size, scrh * 0.5 - half_size, size, size, draw_color)
 
-    draw.RoundedBox(0, scrw * 0.5 - half_size - cur_gap, scrh * 0.5 - half_size, size, size, draw_color)
-    draw.RoundedBox(0, scrw * 0.5 - half_size + cur_gap, scrh * 0.5 - half_size, size, size, draw_color)
+    draw.RoundedBox(0, scrw * 0.5 - half_size - cur_gap, scrh * 0.5 - size, size, double_size, secondary_draw_color)
+    draw.RoundedBox(0, scrw * 0.5 - half_size + cur_gap, scrh * 0.5 - size, size, double_size, secondary_draw_color)
 
-    draw.RoundedBox(0, scrw * 0.5 - half_size, scrh * 0.5 - half_size - cur_gap, size, size, draw_color)
-    draw.RoundedBox(0, scrw * 0.5 - half_size, scrh * 0.5 - half_size + cur_gap, size, size, draw_color)
+    draw.RoundedBox(0, scrw * 0.5 - size, scrh * 0.5 - half_size - cur_gap, double_size, size, secondary_draw_color)
+    draw.RoundedBox(0, scrw * 0.5 - size, scrh * 0.5 - half_size + cur_gap, double_size, size, secondary_draw_color)
   end
 end
 
