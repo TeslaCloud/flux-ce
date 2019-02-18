04e07bf6f4db6ef11cbb38833540c52e0919ef76
AleXXX_007 <AleXXX_007@inbox.ru>
2019-02-18 18:27:14 +0400

    [Characters] Auto replace fix

--- garrysmod/gamemodes/flux/plugins/characters/plugin/lib/sh_character.lua
+++ garrysmod/gamemodes/flux/plugins/characters/plugin/lib/sh_character.lua
@@ -1,13 +1,13 @@
 ﻿library.new 'character'
 
 function character.create(player, data)
-  if (!isstring(data.name) or (data.utf8.len(name) < config.get('character_min_name_len') or
-    data.utf8.len(name) > config.get('character_max_name_len'))) then
+  if (!isstring(data.name) or (utf8.len(data.name) < config.get('character_min_name_len') or
+    utf8.len(data.name) > config.get('character_max_name_len'))) then
     return CHAR_ERR_NAME
   end
 
-  if (!isstring(data.phys_desc) or (data.utf8.len(phys_desc) < config.get('character_min_desc_len') or
-    data.utf8.len(phys_desc) > config.get('character_max_desc_len'))) then
+  if (!isstring(data.phys_desc) or (utf8.len(data.phys_desc) < config.get('character_min_desc_len') or
+    utf8.len(data.phys_desc) > config.get('character_max_desc_len'))) then
     return CHAR_ERR_DESC
   end
 
