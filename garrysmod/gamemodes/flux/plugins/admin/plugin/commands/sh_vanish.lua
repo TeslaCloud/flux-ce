ae5c469b48a0f5e95a399b97b7e301cab228acd2
Alex D <cod7777@yandex.ru>
2019-03-04 23:08:42 +0100

    Vanish command

---
+++ garrysmod/gamemodes/flux/plugins/admin/plugin/commands/sh_vanish.lua
@@ -0,0 +1,53 @@
+local COMMAND = Command.new('vanish')
+COMMAND.name = 'Vanish'
+COMMAND.description = t'vanish.description'
+COMMAND.syntax = t'vanish.syntax'
+COMMAND.permission = 'moderator'
+COMMAND.category = 'categories.player_management'
+COMMAND.arguments = 1
+COMMAND.immunity = true
+COMMAND.aliases = { 'v' }
+
+function COMMAND:on_run(player, targets, should_vanish)
+  for k, target in ipairs(targets) do
+    should_vanish = should_vanish != nil and tobool(should_vanish) or !target.is_vanished
+
+    local phrase = 'vanish.'..(should_vanish and 'enabled' or 'disabled')
+    local target_name = target:Name()
+    local admin_name = get_player_name(player)
+
+    target:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+
+    -- Make sure we are not interfering with observer.
+    if !target:get_nv('observer') then
+      if should_vanish then
+        target.old_color = target:GetColor()
+
+        target:DrawWorldModel(false)
+        target:DrawShadow(false)
+        target:SetNoDraw(true)
+        target:SetNotSolid(true)
+        target:SetColor(Color(0, 0, 0, 0))
+      else
+        target:DrawWorldModel(true)
+        target:DrawShadow(true)
+        target:SetNoDraw(false)
+        target:SetNotSolid(false)
+        target:SetColor(target.old_color)
+      end
+    end
+
+    target:prevent_transmit_conditional(should_vanish, function(ply)
+      if ply:can('moderator') then
+        ply:notify(phrase, { player_name = target_name, admin_name = admin_name })
+        return false
+      end
+    end)
+
+    target:notify('vanish.self', { state = 'vanish.'..(should_vanish and 'vanished' or 'unvanished') })
+
+    target.is_vanished = should_vanish
+  end
+end
+
+COMMAND:register()
---
+++ garrysmod/gamemodes/flux/plugins/admin/plugin/lib/meta/sv_entity.lua
@@ -0,0 +1,23 @@
+local ent_meta = FindMetaTable 'Entity'
+
+function ent_meta:prevent_transmit(target, should_prevent)
+  if IsValid(target) then
+    self:SetPreventTransmit(target, should_prevent)
+
+    for k, child in ipairs(self:GetChildren()) do
+      if IsValid(child) then
+        child:prevent_transmit(target, should_prevent)
+      end
+    end
+  end
+end
+
+function ent_meta:prevent_transmit_conditional(should_prevent, condition)
+  condition = condition or function() return true end
+
+  for k, v in ipairs(player.all()) do
+    if v != self and condition(v, self, should_prevent) != false then
+      self:prevent_transmit(v, should_prevent)
+    end
+  end
+end
