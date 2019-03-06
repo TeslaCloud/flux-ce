0611017a35d8fdfe25c1fa54810ee91c46747cab
Alex D <cod7777@yandex.ru>
2019-03-06 23:21:11 +0100

    Documentation plugin and cratefile format

--- garrysmod/gamemodes/flux/gamemode/lib/active_record/cratefile.lua
+++ garrysmod/gamemodes/flux/gamemode/lib/active_record/cratefile.lua
@@ -1,1 +1,3 @@
+--- @package [ActiveRecord]
+-- ActiveRecord ORM
 include 'ar_shared.lua'
---
+++ garrysmod/gamemodes/flux/plugins/sv_documentation.lua
@@ -0,0 +1,114 @@
+-- WIP WIP WIP
+
+PLUGIN:set_name('Documentation Generator')
+PLUGIN:set_author('TeslaCloud Studios')
+PLUGIN:set_description('Generates documentation for Flux and/or your schema.')
+
+if !fl.development then return end
+
+local openers = {
+  [TK_then] = true,
+  [TK_do] = true
+}
+
+local function extract_functions(code)
+  local tokens = Packager.Lexer:tokenize(code)
+  local func_data = {}
+  local opens = 0
+  local skip_next = false
+  local comments_stack = {}
+  local i = 0
+
+  while i < #tokens do
+    i = i + 1
+    local v = tokens[i]
+
+    -- Ignore anything outside the global level.
+    if openers[v.tk] then
+      opens = opens + 1
+      continue
+    elseif opens > 0 and v.tk == TK_end then
+      opens = opens - 1
+      continue
+    elseif opens > 0 then
+      continue
+    elseif v.tk == TK_comment then
+      table.insert(comments_stack, v)
+      continue
+    end
+
+    if v.tk == TK_local then
+      if tokens[i + 1].tk == TK_function then
+        opens = opens + 1
+        continue
+      end
+    end
+
+    if v.tk == TK_function then
+      if tokens[i + 1].tk != TK_name then
+        opens = opens + 1
+        continue
+      end
+
+      opens = opens + 1
+      i = i + 1
+      v = tokens[i]
+
+      local func_name = ''
+
+      while v and v.tk != TK_lparen do
+        func_name = func_name..v.val
+        i = i + 1
+        v = tokens[i]
+      end
+
+      func_data[func_name] = { name = func_name, comments = comments_stack }
+    end
+
+    comments_stack = {}
+  end
+
+  return func_data
+end
+
+local function extract_functions_from_files(folder)
+  local func_data = {}
+  local files, dirs = file.Find(folder..'*', 'GAME')
+
+  for k, v in ipairs(files) do
+    if v:ends('.lua') then
+      table.Merge(func_data, extract_functions(fileio.Read(folder..v)))
+    end
+  end
+
+  for k, v in ipairs(dirs) do
+    if !v:starts('.') then
+      table.Merge(func_data, extract_functions_from_files(folder..v..'/'))
+    end
+  end
+
+  return func_data
+end
+
+function analyze_folder(folder)
+  print('Analyzing: '..folder)
+
+  local func_data = extract_functions_from_files(folder)
+
+  local undocumented, partial, documented = 0, 0, 0
+
+  for k, v in pairs(func_data) do
+    if #v.comments == 0 then
+      undocumented = undocumented + 1
+    elseif v.comments[1].val:starts('-') then
+      documented = documented + 1
+    else
+      partial = partial + 1
+    end
+  end
+
+  print('Done! Found '..(table.Count(func_data))..' functions.')
+  print('  -> '..(undocumented > 0 and undocumented or 'no')..' undocumented '..(undocumented != 1 and 'functions' or 'function'))
+  print('  -> '..(partial > 0 and partial or 'no')..' partially documented '..(partial != 1 and 'functions' or 'function'))
+  print('  -> '..(documented > 0 and documented or 'no')..' documented '..(documented != 1 and 'functions' or 'function'))
+end
