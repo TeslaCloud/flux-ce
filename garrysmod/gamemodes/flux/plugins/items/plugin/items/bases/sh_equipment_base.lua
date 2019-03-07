975182a2e9e2cd0c5800024093191427186da96f
AleXXX_007 <AleXXX_007@inbox.ru>
2019-03-07 22:52:42 +0400

    [Items] Equipment base finished (hopefully)

--- garrysmod/gamemodes/flux/plugins/items/plugin/items/bases/sh_equipment_base.lua
+++ garrysmod/gamemodes/flux/plugins/items/plugin/items/bases/sh_equipment_base.lua
@@ -38,12 +38,34 @@ end
 
 function ItemEquippable:can_transfer(player, inv_type, x, y)
   if inv_type == self.equip_inv then
+    if self:can_equip(player) == false then
+      return false
+    end
+
     for k, v in pairs(player:get_items(self.equip_inv)) do
       local item_table = item.find_instance_by_id(v)
 
-      if item_table.equip_slot and item_table.equip_slot == self.equip_slot and item_table:is_equipped() and item_table.instance_id != self.instance_id then
-        return false
+      if item_table.equip_slot and item_table:is_equipped() and item_table.instance_id != self.instance_id then
+        if item_table.equip_slot == self.equip_slot then
+          return false
+        elseif istable(self.equip_slot) then
+          for k1, v1 in pairs(self.equip_slot) do
+            if v1 == item_table.equip_slot then
+              return false 
+            elseif istable(item_table.equip_slot) then
+              for k2, v2 in pairs(item_table.equip_slot) do
+                if v1 == v2 then
+                  return false
+                end
+              end
+            end
+          end
+        end
       end
+    end
+  elseif inv_type != self.equip_inv then
+    if self:can_unequip(player) == false then
+      return false
     end
   end
 end
@@ -55,24 +77,20 @@ function ItemEquippable:post_unequipped(player) end
 
 function ItemEquippable:equip(player, should_equip)
   if should_equip then
-    if self:can_equip(player) != false then
-      self:post_equipped(player)
-    end
+    self:post_equipped(player)
   else
-    if self:can_unequip(player) != false then
-      self:post_unequipped(player)
-    end
+    self:post_unequipped(player)
   end
 end
 
 function ItemEquippable:on_inventory_changed(player, new_inv, old_inv)
   if IsValid(player) then
     if new_inv == self.equip_inv then
-      self:equip(player, true)
       player:EmitSound(self.action_sounds['equip'])
+      self:equip(player, true)
     elseif old_inv == self.equip_inv then
-      self:equip(player, false)
       player:EmitSound(self.action_sounds['unequip'])
+      self:equip(player, false)
     end
   end
 end
@@ -89,12 +107,6 @@ function ItemEquippable:on_use(player)
   end
 
   return true
-end
-
-function ItemEquippable:on_drop(player)
-  if self:is_equipped() then
-    self:equip(player, false)
-  end
 end
 
 function ItemEquippable:on_loadout(player)
