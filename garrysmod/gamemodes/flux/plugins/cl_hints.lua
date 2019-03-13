e8fb3cb5618562b022cd913bb7b0287b58078a6f
Alex D <cod7777@yandex.ru>
2019-03-13 01:19:27 +0100

    rename hints

--- garrysmod/gamemodes/flux/plugins/cl_hints.lua
+++ garrysmod/gamemodes/flux/plugins/cl_hints.lua
@@ -1,25 +1,25 @@
-PLUGIN:set_global('flHints')
+PLUGIN:set_global('Hints')
 PLUGIN:set_name('Hints')
 PLUGIN:set_description('Adds hints that are displayed to players.')
 PLUGIN:set_author('TeslaCloud Studios')
 
 local stored = {}
 
-function flHints:OneMinute()
+function Hints:OneMinute()
   local cur_time = CurTime()
 
   if cur_time >= (PLAYER.nextHint or 0) then
-    flHints:display_random()
+    self:display_random()
 
     PLAYER.nextHint = cur_time + 300
   end
 end
 
-function flHints:add(id, text, color, play_sound, callback)
+function Hints:add(id, text, color, play_sound, callback)
   table.insert(stored, { id = id, text = text, color = color, play_sound = play_sound or false, callback = callback })
 end
 
-function flHints:display_random()
+function Hints:display_random()
   local hint = table.Random(stored)
 
   if hint.callback and hint.callback() != true then return end
@@ -29,10 +29,10 @@ function flHints:display_random()
 end
 
 do
-  flHints:add(t('hint.forums'), t('hint.forums_text'))
-  flHints:add(t('hint.hints'), t('hint.hints_text'))
-  flHints:add(t('hint.tab'), t('hint.tab_text'))
-  flHints:add(t('hint.inventory'), t('hint.inventory_text'))
-  flHints:add(t('hint.commands'), t('hint.commands_text'))
-  flHints:add(t('hint.bugs'), t('hint.bugs_text'))
+  Hints:add(t('hint.forums'), t('hint.forums_text'))
+  Hints:add(t('hint.hints'), t('hint.hints_text'))
+  Hints:add(t('hint.tab'), t('hint.tab_text'))
+  Hints:add(t('hint.inventory'), t('hint.inventory_text'))
+  Hints:add(t('hint.commands'), t('hint.commands_text'))
+  Hints:add(t('hint.bugs'), t('hint.bugs_text'))
 end
