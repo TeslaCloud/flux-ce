9b09199f9e89cb4228ef7589f77d298cebc688d3
AleXXX_007 <AleXXX_007@inbox.ru>
2019-04-04 15:38:34 +0400

    [Doors] Don't open menu if no options provided. Also localization

--- garrysmod/gamemodes/flux/plugins/doors/plugin/cl_plugin.lua
+++ garrysmod/gamemodes/flux/plugins/doors/plugin/cl_plugin.lua
@@ -1,25 +1,27 @@
 Cable.receive('fl_door_menu', function(entity, can_lock, conditions)
-  local menu = DermaMenu()
-  local locked = entity:get_nv('fl_locked')
+  if can_lock or can('manage_doors') then
+    local menu = DermaMenu()
+    local locked = entity:get_nv('fl_locked')
 
-  if can_lock then
-    menu:AddOption(locked and 'Unlock' or 'Lock', function()
-      Cable.send('fl_lock_door', entity, !locked)
-    end)
+    if can_lock then
+      menu:AddOption(locked and t'doors.unlock' or t'doors.lock', function()
+        Cable.send('fl_lock_door', entity, !locked)
+      end)
+    end
+
+    if can('manage_doors') then
+      menu:AddOption(t'doors.settings', function()
+        local door_menu = vgui.create('fl_door_menu')
+        door_menu:set_door(entity, conditions)
+      end)
+    end
+
+    if menu:ChildCount() < 1 then return end
+
+    menu:Open()
+    menu:Center()
+
+    menu:MakePopup()
+    menu:DoModal()
   end
-
-  if can('manage_doors') then
-    menu:AddOption('Settings', function()
-      local door_menu = vgui.create('fl_door_menu')
-      door_menu:set_door(entity, conditions)
-    end)
-  end
-
-  if menu:ChildCount() < 1 then return end
-
-  menu:Open()
-  menu:Center()
-
-  menu:MakePopup()
-  menu:DoModal()
 end)
