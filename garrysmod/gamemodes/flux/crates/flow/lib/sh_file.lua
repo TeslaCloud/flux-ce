ad2ea449dc77dea3749fa143d2c3c4e2fc62109d
Alex D <cod7777@yandex.ru>
2019-04-09 20:27:53 +0200

    create more file function aliases

--- garrysmod/gamemodes/flux/crates/flow/lib/sh_file.lua
+++ garrysmod/gamemodes/flux/crates/flow/lib/sh_file.lua
@@ -2,6 +2,27 @@ include 'sh_table.lua'
 
 File = File or {}
 
+File.old_write = File.old_write or File.write
+
+function File.write(file_name, file_contents)
+  local pieces = string.split(file_name, '/')
+  local current_path = ''
+
+  for k, v in ipairs(pieces) do
+    if string.GetExtensionFromFilename(v) != nil then
+      break
+    end
+
+    current_path = current_path..v..'/'
+
+    if !file.Exists(current_path, 'GAME') then
+      File.mkdir(current_path)
+    end
+  end
+
+  File.old_write(file_name, file_contents)
+end
+
 function File.get_list(folder)
   folder = folder:ensure_end('/')
   local files, folders = file.Find(folder..'*', 'GAME')
@@ -24,3 +45,13 @@ function File.get_list(folder)
 
   return table.map(files, function(v) if !b[v] then b[v] = true return v end end)
 end
+
+function File.touch(filename)
+  return File.append(filename, '')
+end
+
+File.rm               = File.delete
+File.create           = File.touch
+File.remove           = File.delete
+File.make_directory   = File.mkdir
+File.create_directory = File.mkdir
--- garrysmod/gamemodes/flux/crates/fluctuations/core/sv_core.lua
+++ garrysmod/gamemodes/flux/crates/fluctuations/core/sv_core.lua
@@ -1,26 +1,5 @@
 DeriveGamemode('sandbox')
 
-File.old_write = File.old_write or File.write
-
-function File.write(file_name, file_contents)
-  local pieces = string.split(file_name, '/')
-  local current_path = ''
-
-  for k, v in ipairs(pieces) do
-    if string.GetExtensionFromFilename(v) != nil then
-      break
-    end
-
-    current_path = current_path..v..'/'
-
-    if !file.Exists(current_path, 'GAME') then
-      File.mkdir(current_path)
-    end
-  end
-
-  File.old_write(file_name, file_contents)
-end
-
 old_server_log = old_server_log or ServerLog
 
 function ServerLog(...)
