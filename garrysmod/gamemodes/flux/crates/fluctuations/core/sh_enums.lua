79bc1a5b0c4bdc8c20dfea9d290e8cd687e706d4
Alex D <cod7777@yandex.ru>
2019-04-15 22:11:56 +0200

    remove deprecated enums

--- garrysmod/gamemodes/flux/crates/fluctuations/core/sh_enums.lua
+++ garrysmod/gamemodes/flux/crates/fluctuations/core/sh_enums.lua
@@ -6,14 +6,6 @@ BAR_TOP                       = 0   -- Bars that are drawn on top of the screen 
 BAR_MANUAL                    = 1   -- Bars that are painted manually (E.G. progress bars).
 BAR_HIDDEN                    = 2   -- Bars that are not currently being drawn.
 
--- File actions
-FILE_ACTION_UNKNOWN           = 0   -- We don't know what happened to the file, but it apparently did.
-FILE_ACTION_ADDED             = 1   -- The file was created.
-FILE_ACTION_REMOVED           = 2   -- The file was removed.
-FILE_ACTION_MODIFIED          = 3   -- The file was modified.
-FILE_ACTION_RENAMED_OLD_NAME  = 4   -- The file was renamed, this is it's old name.
-FILE_ACTION_RENAMED_NEW_NAME  = 5   -- The file was renamed, this is it's new name.
-
 -- Config enums
 CONFIG_PLUGIN                 = 0
 CONFIG_SCHEMA                 = 1
