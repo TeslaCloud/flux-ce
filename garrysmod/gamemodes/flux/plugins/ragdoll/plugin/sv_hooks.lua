712b0c25e83992241502aa239ea4112b3b41d26f
AleXXX_007 <AleXXX_007@inbox.ru>
2019-04-28 17:07:08 +0400

    [Ragdoll] Death while ragdolled fixed

--- garrysmod/gamemodes/flux/plugins/ragdoll/plugin/sv_hooks.lua
+++ garrysmod/gamemodes/flux/plugins/ragdoll/plugin/sv_hooks.lua
@@ -1,7 +1,15 @@
 function PLUGIN:PlayerDeath(player)
+  player:reset_action()
   player:set_ragdoll_state(RAGDOLL_DUMMY)
 end
 
 function PLUGIN:PlayerSpawn(player)
   player:set_ragdoll_state(RAGDOLL_NONE)
+end
+
+function PLUGIN:PlayerThink(player)
+  if !player:Alive() and player:IsFrozen() then
+    print('think')
+    hook.run('PlayerDeathThink', player)
+  end
 end
