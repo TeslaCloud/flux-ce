bd6e4b5df5b6bc5fd5919d9faae01f4b51332606
Alex D <cod7777@yandex.ru>
2019-05-12 21:11:33 +0200

    that's way too many enums

--- plugins/admin/plugin/sh_enums.lua
+++ plugins/admin/plugin/sh_enums.lua
@@ -1,5 +1,5 @@
-PERM_ALLOW                    = 0   -- Allow, means that user has this permission.
-PERM_NO                       = 1   -- Not Set (no), means that this user has no permission.
-PERM_NEVER                    = 2   -- Never, means that user will never have access to this permission, regardless of anything else.
-PERM_ALLOW_OVERRIDE           = 3   -- System's Allow, forces this permission to be "Allow". Overrides "Never".
-PERM_ERROR                    = 999 -- In case something goes wrong.
+PERM_ALLOW            = 0   -- Allow, means that user has this permission.
+PERM_NO               = 1   -- Not Set (no), means that this user has no permission.
+PERM_NEVER            = 2   -- Never, means that user will never have access to this permission, regardless of anything else.
+PERM_ALLOW_OVERRIDE   = 3   -- System's Allow, forces this permission to be "Allow". Overrides "Never".
+PERM_ERROR            = 999 -- In case something goes wrong.
