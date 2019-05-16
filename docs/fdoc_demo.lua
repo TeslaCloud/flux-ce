0aeaeb8cff6f80d782d2018bc980d15920608f91
Luna D <cod7777@yandex.ru>
2019-05-16 23:45:46 +0200

    add fdoc demo

---
+++ docs/fdoc_demo.lua
@@ -0,0 +1,35 @@
+--- Demo all that FDoc has to offer.
+--
+-- Triple ticks will be automatically recognized as an example.
+--  ```
+--  -- Some example. This comment will appear in the example.
+--  test(123, player, { test = true })
+--  ```
+--
+-- You can continue writing description after examples.
+-- All lines that begin with @ will be treated differently.
+--
+-- @warning [Internal] Some custom message to append instead of default message for "Internal".
+-- @deprecation [Text to display in the warning window. Should be deprecation reason.]
+-- @param a [Number] Some number
+-- @param b [Object] Some object
+-- @param c [Hash] Table
+-- @return [Foo blank foo object, Number one hundred]
+--
+-- You can also refer to other functions
+-- @see other_function
+function test(a, b, c)
+  return Foo.new(), 100
+end
+
+--- Demo variable argument functions.
+-- @variant foo(a, b)
+--   @param a [Number] This variant accepts a number as the first parameter
+--   @param b [String] And a string as the second parameter!
+-- @variant foo(a)
+--   @param a [String] If you specify a string here it will do something else!
+-- @return [Nil] You do not have to specify it returns nil, but you can if you want to add a message to it.
+--
+function foo(a, b)
+  -- ...
+end
