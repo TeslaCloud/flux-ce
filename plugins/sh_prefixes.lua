4e576e2c05e99440936e22a272f6e0cad967e853
AleXXX_007 <AleXXX_007@inbox.ru>
2019-06-14 21:13:57 +0400

    [Prefixes] Refactoring

--- plugins/sh_prefixes.lua
+++ plugins/sh_prefixes.lua
@@ -5,16 +5,14 @@ PLUGIN:set_description('Adds prefix adjusting to avoid troubles with certain com
 
 local stored = {}
 
-function Prefixes:StringIsCommand(str)
-  for k, v in ipairs(stored) do
-    if istable(v.prefix) then
-      for k1, v1 in pairs(v.prefix) do
-        if str:utf8lower():starts(v1) then
-          return false
-        end
+function Prefixes:StringIsCommand(text)
+  for k, v in pairs(stored) do
+    local prefix_table = istable(v.prefix) and v.prefix or { v.prefix }
+
+    for k1, v1 in pairs(prefix_table) do
+      if text:utf8lower():starts(v1) then
+        return false
       end
-    elseif str:utf8lower():starts(v.prefix) then
-      return false
     end
   end
 end
@@ -22,34 +20,31 @@ end
 if SERVER then
   function Prefixes:PlayerSay(player, text, team_chat)
     if !string.is_command(text) then
-      for k, v in ipairs(stored) do
-        if istable(v.prefix) then
-          for k2, v2 in ipairs(v.prefix) do
-            if text:utf8lower():starts(v2) or v.check and v.check(text) then
-              local message = text:utf8sub((text:utf8lower():starts(v2) and utf8.len(v2) or 0) + 1)
+      for k, v in pairs(stored) do
+        local prefix_table = istable(v.prefix) and v.prefix or { v.prefix }
 
-              if message != '' then
-                v.callback(player, message, team_chat)
-
-                hook.run('PlayerUsedPrefix', player, v.id, message, team_chat)
-              end
-
-              return ''
-            end
+        for k2, v2 in pairs(prefix_table) do
+          if text:utf8lower():starts(v2) or v.check and v.check(text) then
+            return self:process_prefix(player, k, v2, text, team_chat)
           end
-        elseif text:utf8lower():starts(v.prefix) or v.check and v.check(text) then
-          local message = text:utf8sub((text:utf8lower():starts(v.prefix) and utf8.len(v.prefix) or 0) + 1)
-
-          if message != '' then
-            v.callback(player, message, team_chat)
-
-            hook.run('PlayerUsedPrefix', player, v.id, message, team_chat)
-          end
-
-          return ''
         end
       end
     end
+  end
+
+  function Prefixes:process_prefix(player, prefix_id, prefix, text, team_chat)
+    prefix = prefix or ''
+
+    local prefix_data = stored[prefix_id]
+    local message = text:utf8sub((text:utf8lower():starts(prefix) and utf8.len(prefix) or 0) + 1)
+
+    if message != '' then
+      prefix_data.callback(player, message, team_chat)
+
+      hook.run('PlayerUsedPrefix', player, prefix_id, message, team_chat)
+    end
+
+    return ''
   end
 
   function Prefixes:add(id, data)
