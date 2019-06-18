a48b794ec2c0b565e36b15b2ee8b6ba42c8a032b
AleXXX_007 <AleXXX_007@inbox.ru>
2019-06-18 22:52:48 +0400

    [Inventory] Use only 1-8 hotbar slots

--- plugins/inventory/plugin/sh_hooks.lua
+++ plugins/inventory/plugin/sh_hooks.lua
@@ -9,16 +9,32 @@ function Inventory:PlayerBindPress(player, bind, pressed)
 end
 
 function Inventory:PlayerSelectSlot(player, slot)
-  local cur_time = CurTime()
-  local instance_id = player:get_first_in_slot(slot, 1)
-  local item_table = Item.find_by_instance_id(instance_id)
+  if slot > 1 and slot < 9 then
+    local cur_time = CurTime()
+    local instance_id = player:get_first_in_slot(slot, 1)
+    local item_table = Item.find_by_instance_id(instance_id)
 
-  if !player.next_slot_click or player.next_slot_click <= cur_time then
-    self:popup_hotbar()
+    if !player.next_slot_click or player.next_slot_click <= cur_time then
+      self:popup_hotbar()
 
-    if item_table then
-      if item_table.weapon_class then
-        local weapon = player:GetWeapon(item_table.weapon_class)
+      if item_table then
+        if item_table.weapon_class then
+          local weapon = player:GetWeapon(item_table.weapon_class)
+
+          if IsValid(weapon) then
+            input.SelectWeapon(weapon)
+
+            local active_weapon = player:GetActiveWeapon()
+
+            if IsValid(active_weapon) and active_weapon != weapon then
+              surface.PlaySound('common/wpn_select.wav')
+            end
+          end
+        elseif item_table.on_use then
+          item_table:do_menu_action('on_use')
+        end
+      else
+        local weapon = player:GetWeapon('weapon_fists')
 
         if IsValid(weapon) then
           input.SelectWeapon(weapon)
@@ -26,27 +42,13 @@ function Inventory:PlayerSelectSlot(player, slot)
           local active_weapon = player:GetActiveWeapon()
 
           if IsValid(active_weapon) and active_weapon != weapon then
-            surface.PlaySound('common/wpn_select.wav')
+            surface.PlaySound('common/wpn_hudoff.wav')
           end
         end
-      elseif item_table.on_use then
-        item_table:do_menu_action('on_use')
       end
-    else
-      local weapon = player:GetWeapon('weapon_fists')
 
-      if IsValid(weapon) then
-        input.SelectWeapon(weapon)
-
-        local active_weapon = player:GetActiveWeapon()
-
-        if IsValid(active_weapon) and active_weapon != weapon then
-          surface.PlaySound('common/wpn_hudoff.wav')
-        end
-      end
+      player.next_slot_click = cur_time + 0.2
     end
-
-    player.next_slot_click = cur_time + 0.2
   end
 end
 
