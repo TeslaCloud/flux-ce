80999be70c395f4c0522cdb0ed5818d8989d643b
Luna D <cod7777@yandex.ru>
2019-07-06 12:25:16 +0200

    display core version in devhud

--- plugins/cl_devhud.lua
+++ plugins/cl_devhud.lua
@@ -5,7 +5,8 @@ PLUGIN:set_description('Adds developer HUD.')
 function PLUGIN:HUDPaint()
   if Flux.development then
     if hook.run('HUDPaintDeveloper') == nil then
-      draw.SimpleText('Flux version '..(GAMEMODE.version or 'UNKNOWN')..', development environment.', 'default', 8, ScrH() - 18, Color(200, 100, 100, 200))
+      local flow_version = (Flow and Flow.__crate__ and Flow.__crate__.version) or 'UNKNOWN'
+      draw.SimpleText('Flux version '..(GAMEMODE.version or 'UNKNOWN')..'. Core version '..flow_version..'.', 'default', 8, ScrH() - 18, Color(200, 100, 100, 200))
     end
   end
 end
