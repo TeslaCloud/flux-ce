73b20b575ec4ca69d9704e83fb1e1a44c58431e3
AleXXX_007 <AleXXX_007@inbox.ru>
2019-07-09 01:04:21 +0400

    [Display Typing] Config localization

--- plugins/displaytyping/plugin/sv_plugin.lua
+++ plugins/displaytyping/plugin/sv_plugin.lua
@@ -1,5 +1,3 @@
-Config.set('display_exact_message', true)
-
 Cable.receive('display_typing_text_changed', function(player, new_text)
   if IsValid(player) then
     player:set_nv('chat_text', new_text)
