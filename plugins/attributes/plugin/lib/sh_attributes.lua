14844498129f442a85f77ed0a0e2998ff4239fee
AleXXX_007 <AleXXX_007@inbox.ru>
2019-07-12 16:24:12 +0400

    [Attributes] Boost and multiplier methods

--- plugins/attributes/plugin/lib/sh_attributes.lua
+++ plugins/attributes/plugin/lib/sh_attributes.lua
@@ -44,6 +44,9 @@ function Attributes.register(id, data)
   data.progression_type = data.progression_type or PROGRESSION_GEOMETRIC
   data.progression_coefficient = data.progression_coefficient or 1.2
   data.hidden = data.hidden
+  data.boostable = data.boostable
+  data.multipliable = data.multipliable
+  data.boost_limited = data.boost_limited
 
   hook.run('AttributeRegistered', id, data)
 
@@ -69,21 +72,69 @@ do
 
         if type and v.type != attribute_table.type then continue end
 
-        attributes_table[attribute_id] = {
+        local attribute = {
           level = v.level,
-          progress = v.progress
+          progress = v.progress,
+          boosts = {},
+          multipliers = {}
         }
+
+        for k1, v1 in pairs(v.attribute_boosts) do
+          table.insert(attribute.boosts, {
+            value = v1.value,
+            expires_at = v1.expires_at
+          })
+        end
+
+        for k1, v1 in pairs(v.attribute_multipliers) do
+          table.insert(attribute.multipliers, {
+            value = v1.value,
+            expires_at = v1.expires_at
+          })
+        end
+
+        attributes_table[attribute_id] = attribute
       end
 
       return attributes_table
     end
   end
 
-  function player_meta:get_attribute(attribute_id)
+  function player_meta:get_attribute(attribute_id, no_boost)
     local attribute_table = Attributes.find(attribute_id)
     local attribute = self:get_attributes()[attribute_id]
+    local level = attribute.level or attribute_table.min
+    local boost = (!no_boost and attribute_table.boostable) and self:get_attribute_boost() or 0
 
-    return attribute.level or attribute_table.min, attribute.progress or 0
+    return level + boost, attribute.progress or 0
+  end
+
+  function player_meta:get_attribute_boost(attribute_id)
+    local attribute_table = Attributes.find(attribute_id)
+    local attribute = self:get_attributes()[attribute_id]
+    local boost = 0
+
+    for k, v in pairs(attribute.boosts) do
+      boost = boost + v.value
+    end
+
+    if attribute_table.boost_limited then
+      boost = math.clamp(level, attribute_table.min, attribute_table.max)
+    end
+
+    return boost
+  end
+
+  function player_meta:get_attribute_multiplier(attribute_id)
+    local attribute_table = Attributes.find(attribute_id)
+    local attribute = self:get_attributes()[attribute_id]
+    local multiplier = 0
+
+    for k, v in pairs(attribute.multipliers) do
+      multiplier = multiplier + v.value - 1
+    end
+
+    return math.max(multiplier, 0) + 1
   end
 
   if SERVER then
@@ -120,13 +171,24 @@ do
       self:set_attribute(attribute_id, self:get_attribute(attribute_id) - amount)
     end
 
-    function player_meta:progress_attribute(attribute_id, amount)
+    function player_meta:progress_attribute(attribute_id, amount, no_multiplier)
       local attribute_table = Attributes.find(attribute_id)
       local level, progress = self:get_attribute(attribute_id)
 
       if attribute_table.has_progress then return end
 
-      local char = self:get_character()
+      if attribute_table.multipliable and !no_multiplier then
+        local modifier = self:get_attribute_multiplier(attribute_id)
+
+        if amount < 0 and modifier != 0 then
+          modifier = 1 / modifier
+        end
+
+        amount = math.round(amount * modifier)
+      end
+
+      if amount == 0 then return end
+
       local total_progress = attribute_table:get_total_progress(level)
 
       progress = progress + amount
@@ -151,6 +213,8 @@ do
         progress = 0
       end
 
+      local char = self:get_character()
+
       if char then
         for k, v in pairs(char.attributes) do
           if v.attribute_id == attribute_id then
@@ -168,6 +232,54 @@ do
 
     function player_meta:regress_attribute(attribute_id, amount)
       self:progress_attribute(attribute_id, -amount)
+    end
+
+    function player_meta:boost_attribute(attribute_id, value, duration)
+      local attribute_table = Attributes.find(attribute_id)
+
+      if attribute_table.boostable == false then return end
+
+      for k, v in pairs(self:get_character().attributes) do
+        if v.attribute_id == attribute_id then
+          local boost = AttributeBoost.new()
+            boost.value = value
+            boost.expires_at = to_datetime(os.time() + duration)
+          table.insert(v.attribute_boosts, boost)
+
+          break
+        end
+      end
+
+      local attributes = self:get_nv('attributes')
+        table.insert(attributes[attribute_id].boosts, {
+          value = value,
+          duration = to_datetime(os.time() + duration)
+        })
+      self:set_nv('attributes', attributes)
+    end
+
+    function player_meta:multiply_attribute(attribute_id, value, duration)
+      local attribute_table = Attributes.find(attribute_id)
+
+      if attribute_table.multipliable == false then return end
+
+      for k, v in pairs(self:get_character().attributes) do
+        if v.attribute_id == attribute_id then
+          local multiply = AttributeMultiplier.new()
+            multiply.value = value
+            multiply.expires_at = to_datetime(os.time() + duration)
+          table.insert(v.attribute_multipliers, multiply)
+
+          break
+        end
+      end
+
+      local attributes = self:get_nv('attributes')
+        table.insert(attributes[attribute_id].multipliers, {
+          value = value,
+          duration = to_datetime(os.time() + duration)
+        })
+      self:set_nv('attributes', attributes)
     end
   end
 end
