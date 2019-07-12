2a64b625b0a8f1a0e37b75d03334feb443e5d43a
AleXXX_007 <AleXXX_007@inbox.ru>
2019-07-12 15:07:02 +0400

    [Attribute] Boosts and multipliers models

--- plugins/attributes/plugin/migrations/CreateAttributes.lua
+++ plugins/attributes/plugin/migrations/CreateAttributes.lua
@@ -4,3 +4,15 @@ ActiveRecord.define_model('attributes', function(t)
   t:integer 'level'
   t:integer 'progress'
 end)
+
+ActiveRecord.define_model('attribute_multipliers', function(t)
+  t:integer   'attribute_id'
+  t:integer   'value'
+  t:datetime  'expires_at'
+end)
+
+ActiveRecord.define_model('attribute_boosts', function(t)
+  t:integer   'attribute_id'
+  t:integer   'value'
+  t:datetime  'expires_at'
+end)
--- plugins/attributes/plugin/models/attribute.lua
+++ plugins/attributes/plugin/models/attribute.lua
@@ -1,3 +1,5 @@
 class 'Attribute' extends 'ActiveRecord::Base'
 
-Attribute:belongs_to 'Character'
+Attribute:belongs_to  'Character'
+Attribute:has_many    'attribute_multipliers'
+Attribute:has_many    'attribute_boosts'
