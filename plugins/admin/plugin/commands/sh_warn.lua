1fd2c21663e4957dfcf01158f3b36a4a8169d14b
Price-1 <54769274+Price-1@users.noreply.github.com>
2019-09-20 22:40:17 +0200

    Adding a warn command (#42)
    * Adding a warn command
    
    * Fixed the small problems.
    
    * Added spaces.
    
    * More fixes.
    
    * Fixed all smaller problems.

---
+++ plugins/admin/plugin/commands/sh_warn.lua
@@ -0,0 +1,28 @@
+COMMAND.name = 'Warn'
+COMMAND.description = 'command.warn.description'
+COMMAND.syntax = 'command.warn.syntax'
+COMMAND.permission = 'assistant'
+COMMAND.category = 'permission.categories.player_management'
+COMMAND.arguments = 1
+COMMAND.immunity = true
+COMMAND.aliases = { 'plywarn', 'warn' }
+
+function COMMAND:on_run(player, targets, ...)
+  local reason = table.concat({ ... }, ' ')
+
+  if !reason or reason == '' then
+    player:notify('This is not a valid reason!')
+  else
+    self:notify_staff('command.warn.message', {
+      player = get_player_name(player),
+      target = util.player_list_to_string(targets),
+      reason = reason
+    })
+
+    for k, v in ipairs(targets) do
+      v:notify( "notification.warn", { 
+        reason = reason 
+      })
+    end
+  end
+end
