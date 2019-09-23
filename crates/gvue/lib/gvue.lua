8cf7baa4a628a737edd674d9e9656ec18f3d3442
Luna D <cod7777@yandex.ru>
2019-09-23 23:38:35 +0200

    move element attributes outside the panel

--- crates/gvue/lib/gvue.lua
+++ crates/gvue/lib/gvue.lua
@@ -1,1 +1,4 @@
-require_relative 'gvue/gvue.lua'
+require_relative 'gvue/gvue'
+require_relative 'gvue/element_attributes'
+require_relative 'gvue/element_helpers'
+require_relative 'gvue/elements/basic'
--- crates/gvue/lib/gvue/elements/basic.lua
+++ crates/gvue/lib/gvue/elements/basic.lua
@@ -1,45 +1,4 @@
-local PANEL = {
-  _gvue = {
-    html = {
-      attributes = {},
-      element_name = 'gvue_basic_panel',
-      inner_html = ''
-    },
-    context = {
-      attributes = {
-        padding = '0',
-        padding_up = 0, padding_right = 0, padding_down = 0, padding_left = 0,
-        margin = '0',
-        margin_up = 0, margin_right = 0, margin_down = 0, margin_left = 0,
-        up = 0, down = 0, left = 0, right = 0,
-        background = '0',
-        background_color = nil, background_image = nil,
-        border = '0',
-        border_radius = 0, border_size = 0, border_color = color_white,
-        color = color_white,
-        font_family = 'default',
-        font_size = 16,
-        display = 'block',
-        position = 'relative',
-        width = nil, max_width = nil, min_width = nil,
-        height = nil, max_height = nil, min_height = nil,
-        text_align = 'left',
-        flex = nil,
-        flex_grow = 0, flex_shrink = 0, flex_basis = 0,
-        flex_wrap = 'nowrap', flex_direction = 'row',
-        justify_content = 'flex-start', align_items = 'stretch',
-        align_self = 'auto', align_content = 'stretch',
-        line_height = 0, column_width = 0,
-        order = 0
-      },
-      inner = nil,
-      parent = nil,
-      width = 0,
-      height = 0
-    },
-    scale = 1
-  }
-}
+local PANEL = Gvue.basic_element_attributes
 
 function PANEL:unit_to_px(num, units, what, use_abstract_pixels)
   if !isnumber(num) then return 0 end
