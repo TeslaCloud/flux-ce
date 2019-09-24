667298ca06f6b1d38a32c8112907902774260ff1
Luna D <cod7777@yandex.ru>
2019-09-24 20:59:01 +0200

    update fdoc example

--- docs/fdoc_demo.lua
+++ docs/fdoc_demo.lua
@@ -12,24 +12,25 @@
 -- @warning [Internal] Some custom message to append instead of default message for "Internal".
 -- @deprecation [Text to display in the warning window. Should be deprecation reason.]
 -- @deprecation_version [0.8.0]
--- @param a [Number] Some number
--- @param b [Object] Some object
--- @param c [Hash] Table
+-- @param a=Default Value or Description [Number Some number]
+-- @param b [Object Some object]
+-- @param c [Hash Table]
 -- @return [Foo blank foo object, Number one hundred]
 --
--- You can also refer to other functions
+-- You can also refer to other functions (both variations are valid)
 -- @see other_function
+-- @see [MyClass#method]
 function test(a, b, c)
   return Foo.new(), 100
 end
 
 --- Demo variable argument functions.
 -- @variant foo(a, b)
---   @param a [Number] This variant accepts a number as the first parameter
---   @param b [String] And a string as the second parameter!
+--   @param a [Number This variant accepts a number as the first parameter]
+--   @param b [String And a string as the second parameter!]
 -- @variant foo(a)
---   @param a [String] If you specify a string here it will do something else!
--- @return [Nil] You do not have to specify it returns nil, but you can if you want to add a message to it.
+--   @param a [String If you specify a string here it will do something else!]
+-- @return [Nil You do not have to specify this explicitly if it returns nil, but you can if you want to add a message to it.]
 --
 function foo(a, b)
   -- ...
