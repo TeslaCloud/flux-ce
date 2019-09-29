65583ca1b8927d5fa5e877b808f20db6ea436039
AleXXX_007 <AleXXX_007@inbox.ru>
2019-09-29 16:17:58 +0400

    [UI] Make fl_frame draggable

--- crates/flow/views/base/cl_frame.lua
+++ crates/flow/views/base/cl_frame.lua
@@ -1,4 +1,5 @@
 local PANEL = {}
+PANEL.draggable = true
 
 function PANEL:Init()
   local padding = math.scale(4)
@@ -11,10 +12,15 @@ function PANEL:Init()
   self.button_close:SetPos(0, 0)
   self.button_close:set_icon('fa-times')
   self.button_close:set_text('')
+  self.button_close:set_centered(true)
   self.button_close:SetDrawBackground(false)
   self.button_close.DoClick = function(btn)
     self:safe_remove()
   end
+end
+
+function PANEL:PerformLayout(w, h)
+  self.button_close:SetPos(w - math.scale_x(20), 0)
 end
 
 function PANEL:Paint(w, h)
@@ -22,13 +28,41 @@ function PANEL:Paint(w, h)
 end
 
 function PANEL:Think()
-  local w, h = self:GetSize()
+  if self.dragging then
+    local scrw, scrh = ScrW(), ScrH()
+    local mouse_x = math.clamp(gui.MouseX(), 1, scrw - 1)
+    local mouse_y = math.clamp(gui.MouseY(), 1, scrh - 1)
+    local x, y = mouse_x - self.dragging[1], mouse_y - self.dragging[2]
 
-  if IsValid(self.button_close) then
-    self.button_close:SetPos(w - 20, 0)
+    x = math.clamp(x, 0, scrw - self:GetWide())
+    y = math.clamp(y, 0, scrh - self:GetTall())
+
+    self:SetPos(x, y)
   end
 
   Theme.hook('FrameThink')
+end
+
+function PANEL:OnMousePressed()
+  if self:is_draggable() then
+    self.dragging = { gui.MouseX() - self.x, gui.MouseY() - self.y }
+    self:MouseCapture(true)
+  end
+end
+
+function PANEL:OnMouseReleased()
+  if self:is_draggable() then
+    self.dragging = nil
+    self:MouseCapture(false)
+  end
+end
+
+function PANEL:set_draggable(bool)
+  self.draggable = true
+end
+
+function PANEL:is_draggable()
+  return self.draggable
 end
 
 vgui.Register('fl_frame', PANEL, 'fl_base_panel')
