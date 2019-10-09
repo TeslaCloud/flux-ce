43a8893a86c7f43790b350f2929433e5924c2359
Luna D <cod7777@yandex.ru>
2019-10-09 18:49:10 -0400

    draw boundaries too

--- crates/gvue/lib/gvue/elements/basic.lua
+++ crates/gvue/lib/gvue/elements/basic.lua
@@ -1,10 +1,12 @@
 local PANEL = Gvue.basic_element_attributes
 
 local debug_colors = {
-  pure_red        = Color(255, 0, 0),
-  transparent_red = Color(255, 0, 0, 75),
-  pure_white      = Color(255, 255, 255),
-  gray            = Color(125, 125, 125)
+  border  = Color(50, 100, 150),
+  fill    = Color(50, 100, 150, 60),
+  tooltip = Color(255, 255, 255),
+  text    = Color(125, 125, 125),
+  margin  = Color(200, 150, 50, 90),
+  padding = Color(200, 50, 125, 90)
 }
 
 function PANEL:Paint(w, h)
@@ -29,24 +31,62 @@ function PANEL:Paint(w, h)
   end
 
   if self.draw_debug_overlay then
-    -- Red fill
-    surface.set_draw_color(debug_colors.transparent_red)
-    surface.draw_rect(0, 0, w, h)
-    
+    local attrs = self.context.attributes
+    surface.DisableClipping(true)
+
+    -- Background fill
+    surface.set_draw_color(debug_colors.fill)
+    surface.draw_rect(
+      attrs.padding_left,
+      attrs.padding_top,
+      w - attrs.padding_right - attrs.padding_left - 1,
+      h - attrs.padding_top - attrs.padding_bottom - 1
+    )
+
+    -- Draw margin boundaries
+    surface.set_draw_color(debug_colors.margin)
+
+    draw.stenciled(function()
+      surface.draw_rect(
+        -attrs.margin_left,
+        -attrs.margin_top,
+        w + attrs.margin_left + attrs.margin_right,
+        h + attrs.margin_top + attrs.margin_bottom
+      )
+    end, function()
+      surface.draw_rect(0, 0, w, h)
+    end)
+
+    -- Draw padding boundaries
+    surface.set_draw_color(debug_colors.padding)
+
+    draw.stenciled(function()
+      surface.draw_rect(0, 0, w, h)
+    end, function()
+      surface.draw_rect(
+        attrs.padding_left,
+        attrs.padding_top,
+        w - attrs.padding_right - attrs.padding_left - 1,
+        h - attrs.padding_top - attrs.padding_bottom - 1
+      )
+    end)
+
     -- Draw overlaid lines
-    surface.set_draw_color(debug_colors.pure_red)
+    surface.set_draw_color(debug_colors.border)
 
     -- Order as follows: top right bottom left
     surface.draw_line(0, 0, w, 0)
-    surface.draw_line(w, 0, w, h)
-    surface.draw_line(0, h, w, h)
+    surface.draw_line(w - 1, 0, w - 1, h)
+    surface.draw_line(0, h - 1, w, h - 1)
     surface.draw_line(0, 0, 0, h)
 
     -- Overlay text and background box
     local panel_info_text = tostring(self.html.element_name)..' '..tostring(w)..'x'..tostring(h)
     local text_wide, text_tall = util.text_size(panel_info_text, 'default')
-    draw.rounded_box(4, 1, 1, text_wide + 8, text_tall + 8, debug_colors.pure_white)
-    draw.simple_text(panel_info_text, 'default', 5, 5, debug_colors.gray)
+    draw.rounded_box(4, 1, 1, text_wide + 8, text_tall + 8, debug_colors.tooltip)
+    draw.simple_text(panel_info_text, 'default', 5, 5, debug_colors.text)
+
+    surface.DisableClipping(false)
   end
 end
 
@@ -67,3 +107,19 @@ function PANEL:set_padding(up, right, down, left)
 end
 
 vgui.Register('gvue_basic_panel', PANEL, 'EditablePanel')
+
+concommand.Add('gvue_test', function()
+  if IsValid(__GVUE_PANE__) then
+    __GVUE_PANE__:SetVisible(false)
+    __GVUE_PANE__:Remove()
+  end
+
+  __GVUE_PANE__ = vgui.Create('gvue_basic_panel')
+  __GVUE_PANE__:SetSize(512, 256)
+  __GVUE_PANE__:SetPos(64, 64)
+end)
+
+concommand.Add('gvue_close', function()
+  __GVUE_PANE__:SetVisible(false)
+  __GVUE_PANE__:Remove()
+end)
