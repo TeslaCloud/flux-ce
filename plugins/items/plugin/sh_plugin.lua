49c2c64d3162fc7f6e2637c972731fab6f0525ae
Luna D <cod7777@yandex.ru>
2019-10-12 08:08:36 -0400

    fix item bases being loaded after items

--- plugins/items/plugin/sh_plugin.lua
+++ plugins/items/plugin/sh_plugin.lua
@@ -5,8 +5,8 @@ require_relative 'sv_hooks'
 require_relative 'sh_enums'
 
 function Items:OnPluginLoaded()
-  Plugin.add_extra('items')
   Plugin.add_extra('items/bases')
+  Plugin.add_extra('items')
 
   require_relative_folder(self:get_folder()..'/items/bases')
   Item.include_items(self:get_folder()..'/items/')
