cd9952b26b6d4afeed4b057e845015980f074caf
AleXXX_007 <AleXXX_007@inbox.ru>
2019-10-20 10:13:15 +0400

    [General] Entity interaction menu

--- crates/flow/hooks/cl_nethooks.lua
+++ crates/flow/hooks/cl_nethooks.lua
@@ -27,14 +27,15 @@ Cable.receive('fl_player_take_damage', function()
 end)
 
 Cable.receive('fl_player_interact', function(target)
-  local player_menu = DermaMenu()
+Cable.receive('fl_entity_interact', function(target)
+  local interaction_menu = DermaMenu()
 
-  hook.run('CreatePlayerInteractions', player_menu, target)
+  hook.run('CreateEntityInteractions', interaction_menu, target)
 
-  if player_menu:ChildCount() > 0 then
-    player_menu:Open()
-    player_menu:Center()
+  if interaction_menu:ChildCount() > 0 then
+    interaction_menu:Open()
+    interaction_menu:Center()
   else
-    player_menu:safe_remove()
+    interaction_menu:safe_remove()
   end
 end)
